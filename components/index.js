8e436167ebad368fc872e8addf07608548ba1bc0
garyxdh <garyxdh@gmail.com>
2023-11-27 19:06:56 +0800

    feat: 删除多余代码

--- components/index.js
+++ components/index.js
@@ -1,21 +1,6 @@
-import AddImage from './AddImage.vue';
-import AgreementDialog from './AgreementDialog.vue';
-import DatePicker from './DatePicker.vue';
-import Dialog from './Dialog.vue';
-import FJImage from './FJImage.vue';
-import SendCode from './SendCode.vue';
-import Tabbar from './Tabbar.vue';
-import Tabs from './Tabs.vue';
-import UpgradeDialog from './UpgradeDialog.vue';
+import ClassifyCards from "./ClassifyCards.vue";
+import CenterPanel from "./CenterPanel.vue";
+import GridPanel from "./GridPanel.vue";
+import RecommendedList from "./RecommendedList.vue";
 
-export {
-  AddImage,
-  AgreementDialog,
-  DatePicker,
-  Dialog,
-  FJImage,
-  SendCode,
-  Tabbar,
-  Tabs,
-  UpgradeDialog
-}
+export { ClassifyCards, CenterPanel, GridPanel, RecommendedList };
