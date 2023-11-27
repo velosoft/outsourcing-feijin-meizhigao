de9535abe0d7ef32c1569c791e363ab87ad6e4b6
Ronin <42492283+joshuabsyxzry@users.noreply.github.com>
2023-11-27 20:38:33 +0800

    feat: 修改首页类名

--- pages/tabBar/home.vue
+++ pages/tabBar/home.vue
@@ -1,64 +1,63 @@
 <template>
-  <view class="flex-col flex-1 group_2">
-    <view class="flex-row items-center group_24">
-      <view class="flex-row items-center flex-1 section_2">
-        <image
-          class="image_7"
-          src="https://dev.ft.velosoft.cn/api/image?token=6564447bcc0204001240f5d1&name=5fcc6916a5ab113d1b3ddcc34f70cac3.png"
-        />
-        <text class="ml-6 text_4">搜索</text>
-      </view>
-      <view class="ml-8 flex-col items-center shrink-0 relative group">
-        <image
-          class="image_6"
-          src="https://dev.ft.velosoft.cn/api/image?token=6564447bcc0204001240f5d1&name=ffeac1ccce1af8bba808c53471f46788.png"
-        />
-        <image
-          class="image_5 pos"
-          src="https://dev.ft.velosoft.cn/api/image?token=6564447bcc0204001240f5d1&name=4500ef7a4d5e93d0a4af0516550ee7bd.png"
-        />
-      </view>
-    </view>
-    <view class="flex-row justify-end section_3">
-      <view class="section_4"></view>
-      <view class="ml-4 section_5"></view>
-      <view class="ml-4 section_5"></view>
-    </view>
-    <view class="flex-row items-center section_6">
+<view class="flex-col flex-1 home-con">
+  <view class="flex-row items-center search-box">
+    <view class="flex-row items-center flex-1 section_2">
       <image
-        class="image_5"
-        src="https://dev.ft.velosoft.cn/api/image?token=6564447bcc0204001240f5d1&name=747a5f574dd1d31f41e2124c80439bc9.png"
+        class="image_7"
+        src="https://dev.ft.velosoft.cn/api/image?token=6564447bcc0204001240f5d1&name=5fcc6916a5ab113d1b3ddcc34f70cac3.png"
       />
-      <text class="ml-10 font_2 text_5"
-        >显示最新一条系统公告的标题，系统公告的标题…</text
-      >
+      <text class="ml-6 text_4">搜索</text>
     </view>
-    <ClassifyCards class="group_22"></ClassifyCards>
-    <CenterPannel class="group_22"></CenterPannel>
-    <GridPanel class="group_8"></GridPanel>
-    <view class="flex-col group_28">
-      <view class="flex-col section_8">
-        <view
-          class="flex-row justify-between items-center self-stretch group_13"
-        >
-          <view class="flex-col items-start">
-            <text class="text_23">新会员入会礼包</text>
-            <text class="CenterPannel-mt-8 CenterPannel-font text_24"
-              >你有一份价值100元的新人礼包</text
-            >
-          </view>
-          <view class="flex-col items-center text-wrapper_2"
-            ><text class="a-i-outflow">待领取</text></view
-          >
-        </view>
-        <view class="self-stretch divider"></view>
-        <text class="self-start CenterPannel-font text_25"
-          >2023.07.6-2023.08.12</text
-        >
+    <view class="ml-8 flex-col items-center shrink-0 relative search-right">
+      <image
+        class="message"
+        src="https://dev.ft.velosoft.cn/api/image?token=6564447bcc0204001240f5d1&name=ffeac1ccce1af8bba808c53471f46788.png"
+      />
+      <view class="flex-col justify-start items-center dot-wrap dot-pos">
+        <image
+          class="notify-image"
+          src="https://project-user-resource-1256085488.cos.ap-guangzhou.myqcloud.com/5f994f8347e00b001139c3d4/6564419acc0204001240f4ec/17010692422372301351.png"
+        />
+        <text class="home-font_03 dot-text dot-pos-text">1</text>
       </view>
     </view>
-    <RecommendedList class="section_13"></RecommendedList>
   </view>
+  <view class="flex-row justify-end swiper-box">
+    <view class="section_4"></view>
+    <view class="ml-4 section_5"></view>
+    <view class="ml-4 section_5"></view>
+  </view>
+  <view class="flex-row items-center notify-box">
+    <image
+      class="notify-image"
+      src="https://dev.ft.velosoft.cn/api/image?token=6564447bcc0204001240f5d1&name=747a5f574dd1d31f41e2124c80439bc9.png"
+    />
+    <text class="ml-10 home-font_02 notify-text">显示最新一条系统公告的标题，系统公告的标题…</text>
+  </view>
+  <ClassifyCards class="group_22"></ClassifyCards>
+  <CenterPannel class="group_22"></CenterPannel>
+  <GridPanel class="group_8"></GridPanel>
+  <view class="flex-col member-box">
+    <view class="flex-col justify-start relative member-section">
+      <image
+        class="member-bg member-pos"
+        src="https://dev.ft.velosoft.cn/api/image?token=6564447bcc0204001240f5d1&name=484db8f69476ecb8a61eb74c858fcdd9.png"
+      />
+      <view class="flex-col relative">
+        <view class="flex-row justify-between items-center self-stretch member-group">
+          <view class="flex-col items-start">
+            <text class="member-text">新会员入会礼包</text>
+            <text class="CenterPannel-mt-8 CenterPannel-font member-describe">你有一份价值100元的新人礼包</text>
+          </view>
+          <view class="flex-col items-center member-text-wrapper"><text class="btn-member">待领取</text></view>
+        </view>
+        <view class="self-stretch member-divider"></view>
+        <text class="self-start CenterPannel-font member-time">2023.07.6-2023.08.12</text>
+      </view>
+    </view>
+  </view>
+  <RecommendedList class="section_13"></RecommendedList>
+</view>
 </template>
 
 <script>
@@ -79,123 +78,150 @@ export default {
 </script>
 
 <style scoped lang="less">
-.group_2 {
+.home-con {
   overflow-y: auto;
-  .group_24 {
-    padding: 0.75rem 0.88rem 1.25rem;
+  .search-box {
+    padding: 24rpx 28rpx 40rpx;
     .section_2 {
-      padding: 0.5rem 1rem;
+      padding: 16rpx 32rpx;
       background-color: #f0f0f0;
-      border-radius: 3.13rem;
-      height: 2.13rem;
+      border-radius: 100rpx;
+      height: 68rpx;
       .image_7 {
-        width: 1.02rem;
-        height: 0.96rem;
+        width: 32.68rpx;
+        height: 30.7rpx;
       }
       .text_4 {
         color: #a7a7a7;
-        font-size: 0.81rem;
+        font-size: 26rpx;
         font-family: PingFang SC Bold;
         font-weight: 700;
-        line-height: 0.81rem;
+        line-height: 26rpx;
       }
     }
-    .group {
-      padding-top: 0.25rem;
-      width: 2.38rem;
-      .image_6 {
+    .search-right {
+      padding-top: 8rpx;
+      width: 76rpx;
+      .message {
         border-radius: 50%;
-        width: 2.13rem;
-        height: 2.13rem;
+        width: 68rpx;
+        height: 68rpx;
       }
-      .pos {
+      .dot-wrap {
+        background-color: #ffffff00;
+        width: 30rpx;
+        .home-font_03 {
+          font-size: 20rpx;
+          font-family: PingFang SC;
+          line-height: 20rpx;
+        }
+        .dot-text {
+          color: #ffffff;
+          line-height: 14.28rpx;
+        }
+        .dot-pos-text {
+          position: absolute;
+          left: 50%;
+          top: 50%;
+          transform: translate(-50%, -50%);
+        }
+      }
+      .dot-pos {
         position: absolute;
         right: 0;
         top: 0;
       }
     }
   }
-  .section_3 {
-    padding: 15.25rem 0.63rem 0.75rem;
-    background-image: url("https://dev.ft.velosoft.cn/api/image?token=6564447bcc0204001240f5d1&name=fc726dc031e684bc3430d7da9a9eef75.png");
+  .swiper-box {
+    padding: 488rpx 20rpx 24rpx;
+    background-image: url('https://dev.ft.velosoft.cn/api/image?token=6564447bcc0204001240f5d1&name=fc726dc031e684bc3430d7da9a9eef75.png');
     background-size: 100% 100%;
     background-repeat: no-repeat;
     .section_4 {
       background-color: #ffffff;
-      border-radius: 0.63rem;
-      width: 1.13rem;
-      height: 0.13rem;
+      border-radius: 20rpx;
+      width: 36rpx;
+      height: 4rpx;
     }
     .section_5 {
       background-color: #ffffff80;
-      border-radius: 0.63rem;
-      width: 1.13rem;
-      height: 0.13rem;
+      border-radius: 20rpx;
+      width: 36rpx;
+      height: 4rpx;
     }
   }
-  .section_6 {
-    padding: 0.75rem 1rem;
+  .notify-box {
+    padding: 24rpx 32rpx;
     background-color: #f7f7f799;
-    .font_2 {
-      font-size: 0.75rem;
+    .home-font_02 {
+      font-size: 24rpx;
       font-family: 苹方;
-      line-height: 0.75rem;
+      line-height: 24rpx;
       color: #030305;
     }
-    .text_5 {
+    .notify-text {
       color: #6c6c6c;
     }
   }
-  .image_5 {
-    width: 0.94rem;
-    height: 0.94rem;
+  .notify-image {
+    width: 30rpx;
+    height: 30rpx;
   }
   .group_22 {
-    margin: 1.75rem 1rem 0;
+    margin: 56rpx 32rpx 0;
   }
   .group_8 {
-    margin: 2rem 1rem 0;
+    margin: 64rpx 32rpx 0;
   }
-  .group_28 {
-    margin: 1.25rem 1rem 0;
-    .section_8 {
-      padding-bottom: 0.25rem;
-      background-image: url("https://dev.ft.velosoft.cn/api/image?token=6564447bcc0204001240f5d1&name=484db8f69476ecb8a61eb74c858fcdd9.png");
-      background-position: 0% 0%;
-      background-size: 21.56rem 5.81rem;
-      background-repeat: no-repeat;
-      .group_13 {
-        padding: 1rem 0.75rem 1rem 1.25rem;
-        .text_23 {
+  .member-box {
+    margin: 40rpx 32rpx 0;
+    .member-section {
+      padding-bottom: 8rpx;
+      height: 186rpx;
+      .member-bg {
+        width: 92vw;
+        height: 24.8vw;
+      }
+      .member-pos {
+        position: absolute;
+        left: 0;
+        right: 0;
+        top: 0;
+        bottom: 0;
+      }
+      .member-group {
+        padding: 32rpx 24rpx 32rpx 40rpx;
+        .member-text {
           color: #bb3e0c;
-          font-size: 1rem;
+          font-size: 32rpx;
           font-family: HarmonyOSSansSC;
           font-weight: 700;
-          line-height: 1rem;
+          line-height: 32rpx;
         }
         .CenterPannel-mt-8 {
-          margin-top: 0.5rem;
+          margin-top: 16rpx;
         }
-        .text_24 {
-          font-size: 0.69rem;
-          line-height: 0.69rem;
+        .member-describe {
+          font-size: 22rpx;
+          line-height: 22rpx;
         }
-        .text-wrapper_2 {
-          padding: 0.5rem 0;
+        .member-text-wrapper {
+          padding: 16rpx 0;
           background-color: #bb3e0c;
-          border-radius: 0.25rem;
-          width: 3.75rem;
-          height: 1.63rem;
-          .a-i-outflow {
+          border-radius: 8rpx;
+          width: 120rpx;
+          height: 52rpx;
+          .btn-member {
             color: #ffffff;
-            font-size: 0.75rem;
+            font-size: 24rpx;
             font-family: HarmonyOSSansSC;
             font-weight: 700;
-            line-height: 0.75rem;
+            line-height: 24rpx;
           }
         }
       }
-      .divider {
+      .member-divider {
         background-image: repeating-linear-gradient(
           90deg,
           #ead2ca,
@@ -203,26 +229,26 @@ export default {
           transparent 0.58%,
           transparent 1.161%
         );
-        background-position: -0.063rem 0rem;
-        height: 0.063rem;
+        background-position: -2rpx 0rpx;
+        height: 2rpx;
       }
       .CenterPannel-font {
-        font-size: 0.63rem;
+        font-size: 20rpx;
         font-family: 苹方;
-        line-height: 0.63rem;
+        line-height: 20rpx;
         color: #bb3e0c;
       }
-      .text_25 {
-        margin-left: 1.5rem;
-        margin-top: 0.75rem;
-        line-height: 0.75rem;
+      .member-time {
+        margin-left: 48rpx;
+        margin-top: 24rpx;
+        line-height: 24rpx;
         text-align: center;
         word-break: break-all;
       }
     }
   }
   .section_13 {
-    margin-top: 1.38rem;
+    margin-top: 44rpx;
   }
 }
 </style>
