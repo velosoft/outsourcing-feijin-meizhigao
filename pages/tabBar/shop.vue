3ad55c07276b1bb39d74564d80c2f43747e7e018
Ronin <42492283+joshuabsyxzry@users.noreply.github.com>
2023-11-28 11:16:50 +0800

    feat: 优化商城页

--- pages/tabBar/shop.vue
+++ pages/tabBar/shop.vue
@@ -10,50 +10,50 @@
             :key="index"
           >
             <view
-              class="flex-row group_22"
+              class="flex-row sider-bar-mar"
               :class="{ active: isActive === index }"
               @click="checked(index)"
             >
-              <view :class="{ section_3: isActive === index }"></view>
-              <text class="ml-20 sider-bar-text" :class="{ active: isActive === index }">{{ item.tit }}</text>
+              <view :class="{ line: isActive === index }"></view>
+              <text
+                class="ml-20 sider-bar-text"
+                :class="{ active: isActive === index }"
+                >{{ item.tit }}</text
+              >
             </view>
           </scroll-view>
         </view>
       </view>
     </view>
-    <view class="flex-col flex-1 group_5">
+    <view class="flex-col flex-1 right-panel">
       <view class="flex-col justify-start swiper-wrap">
-        <view class="flex-col justify-start items-center">
-          <image
-            class="a-i-background-image"
-            src="https://dev.ft.velosoft.cn/api/image?token=6564c3e6cc0204001241016e&name=b707d6e23925649ad73479dac900e0e5.png"
-          />
-        </view>
-        <view class="flex-row justify-evenly image-wrapper pos_4">
-          <view class="section_5"></view>
-          <view class="section_6"></view>
-          <view class="section_6"></view>
-          <view class="section_6"></view>
-        </view>
+        <swiper
+          class="right-swiper"
+          style="height: 154rpx; width: 480rpx"
+          autoplay="true"
+          indicator-dots="true"
+        >
+          <swiper-item v-for="(item, index) in Swiperitems" :key="index">
+            <image
+              class="swiper-image"
+              src="https://dev.ft.velosoft.cn/api/image?token=6564c3e6cc0204001241016e&name=b707d6e23925649ad73479dac900e0e5.png"
+            />
+          </swiper-item>
+        </swiper>
       </view>
       <view class="mt-16 flex-row tabs-wrap">
-        <view class="flex-col justify-start items-center shrink-0 text-wrapper"
-          ><text class="text_7">全部</text></view
-        >
         <view
-          class="ml-6 flex-col justify-start items-center shrink-0 text-wrapper_2"
+          class="u-page__tag-item"
+          v-for="(item, index) in radios"
+          :key="index"
         >
-          <text class="font_4">塑料材质</text>
-        </view>
-        <view
-          class="ml-6 flex-col justify-start items-center shrink-0 text-wrapper_3"
-        >
-          <text class="font_4">铁质收纳盒</text>
-        </view>
-        <view
-          class="ml-6 flex-col justify-start items-start shrink-0 text-wrapper_4"
-        >
-          <text class="font_4 text_10">小物件收纳盒</text>
+          <u-tag
+            :text="item.value"
+            :plain="!item.checked"
+            :name="index"
+            @click="radioClick"
+          >
+          </u-tag>
         </view>
       </view>
       <view class="mt-16 flex-col list-wrap">
@@ -70,21 +70,21 @@
             <text class="title">衣架防滑无痕围巾架丝</text>
             <view class="flex-row tag items-center">
               <view
-                class="text-wrapper_10 items-center flex-row"
+                class="tag-wrap items-center flex-row"
                 v-for="(item, index) in items_1"
                 :key="index"
               >
-                <text class="font_5 text_14">新品上市</text>
+                <text class="tag-text">新品上市</text>
               </view>
             </view>
             <view class="flex-row items-center shop-con-bottom">
-              <text class="font_6 text_16">定金</text>
+              <text class="font-con_01 deposit-text">定金</text>
               <view class="flex-row shrink-0">
-                <text class="self-start text_17">￥</text>
-                <text class="font_7 text_18">200</text>
-                <text class="self-center font_8 text_19">.00</text>
+                <text class="self-start price-symbol">￥</text>
+                <text class="price-number">200</text>
+                <text class="self-center decimal-font">.00</text>
               </view>
-              <text class="font_6 text_20">100人预约</text>
+              <text class="font-con_01 pre-book">100人预约</text>
             </view>
           </view>
         </view>
@@ -110,12 +110,36 @@ export default {
         { index: 4, tit: "卧室场景" },
         { index: 5, tit: "客厅场景" },
       ],
+      Swiperitems: [null, null, null, null],
+      radios: [
+        {
+          checked: true,
+          value: "全部",
+        },
+        {
+          checked: false,
+          value: "塑料材质",
+        },
+        {
+          checked: false,
+          value: "铁质收纳盒",
+        },
+        {
+          checked: false,
+          value: "小物件收纳盒",
+        },
+      ],
     };
   },
 
   methods: {
     checked(index) {
       this.isActive = index;
+    },
+    radioClick(name) {
+      this.radios.map((item, index) => {
+        item.checked = index === name ? true : false;
+      });
     },
   },
 };
@@ -136,7 +160,7 @@ export default {
   width: 206rpx;
   height: 108rpx;
 }
-.sider-bar-text{
+.sider-bar-text {
   height: 108rpx;
   font-size: 28rpx;
   font-family: 苹方;
@@ -149,105 +173,77 @@ export default {
   color: #030305;
 }
 
-.group_22 {
+.sider-bar-mar {
   padding: 40rpx 0;
-  .section_3 {
+  .line {
     background-color: #b09053;
     width: 6rpx;
     height: 24rpx;
   }
 }
-.section_4 {
-  padding: 32rpx 48rpx 712rpx;
-  background-color: #f8f8f8;
-  border-radius: 0rpx 8rpx 0rpx 0rpx;
-  .font_3 {
-    font-size: 28rpx;
-    font-family: 苹方;
-    line-height: 28rpx;
-    color: #6c6c6c;
-  }
-}
-.group_5 {
+.right-panel {
   padding: 32rpx 32rpx 0;
   height: 1216rpx;
   overflow-y: auto;
   .swiper-wrap {
     position: relative;
     align-self: stretch;
-    .a-i-background-image {
+    .swiper-image {
       width: 480rpx;
       height: 154rpx;
     }
-    .image-wrapper {
-      width: 88rpx;
-      .section_5 {
-        background-color: #ffffff;
-        border-radius: 8rpx;
-        width: 34rpx;
-        height: 10rpx;
-      }
-      .section_6 {
-        background-color: #ffffff;
-        border-radius: 8rpx;
-        width: 10rpx;
-        height: 10rpx;
-      }
+  }
+  .right-swiper {
+    /deep/ .uni-swiper-dots {
+      // 指示点整个区域
+      bottom: 8rpx;
     }
-    .pos_4 {
-      position: absolute;
-      left: 50%;
-      top: 136rpx;
-      transform: translateX(-50%);
+    /deep/ .uni-swiper-dot {
+      // 指示点元素默认样式
+      background-color: #ffffff;
+      border-radius: 8rpx;
+      width: 10rpx;
+      height: 10rpx;
+      margin-right: 8rpx;
+    }
+    /deep/ .uni-swiper-dot-active {
+      // 指示点元素激活（当前选中）状态样式
+      background-color: #ffffff;
+      border-radius: 8rpx;
+      width: 34rpx;
+      height: 10rpx;
     }
   }
   .tabs-wrap {
     overflow-x: hidden;
-    .text-wrapper {
-      padding: 16rpx 0;
+    /deep/ .u-tag {
+      border: unset !important;
+
+      border-radius: 8rpx;
+      height: 54rpx;
       background-color: #b09053;
-      border-radius: 8rpx;
-      width: 84.76rpx;
-      height: 54rpx;
-      .text_7 {
-        color: #ffffff;
-        font-size: 24rpx;
-        font-family: PingFangSC-Medium;
-        line-height: 24rpx;
-      }
     }
-    .text-wrapper_2 {
-      padding: 16rpx 0;
+    /deep/ .u-tag--primary--plain {
       background-color: #f8f8f8;
-      border-radius: 8rpx;
-      width: 136rpx;
-      height: 54rpx;
     }
-    .text-wrapper_3 {
-      padding: 16rpx 0;
-      background-color: #f8f8f8;
-      border-radius: 8rpx;
-      width: 160rpx;
-      height: 54rpx;
+    /deep/ .u-tag__text {
+      color: #ffffff;
+      font-size: 24rpx;
+      font-family: PingFangSC-Medium;
+      line-height: 24rpx;
     }
-    .font_4 {
+    /deep/ .u-tag__text--primary--plain {
       font-size: 24rpx;
       font-family: 苹方;
       line-height: 24rpx;
       color: #6c6c6c;
     }
-    .text-wrapper_4 {
-      padding: 16rpx 0;
-      background-color: #f8f8f8;
-      border-radius: 8rpx;
-      height: 54rpx;
-      .text_10 {
-        margin-left: 24rpx;
-      }
+    /deep/ .u-page__tag-item {
+      margin-right: 12rpx;
     }
   }
   .list-wrap {
-    width: 474.6rpx;
+    width: 474rpx;
     .list-item {
       &:first-child {
         margin-top: 0;
@@ -260,63 +256,55 @@ export default {
         margin: 8rpx 0 8rpx 24rpx;
         .tag {
           margin-top: 16rpx;
-          .text-wrapper_10 {
+          .tag-wrap {
             padding: 0 8rpx;
             background-image: url("https://dev.ft.velosoft.cn/api/image?token=6564c3e6cc0204001241016e&name=9a3b9a726ef2d84aeb76065d090ea380.png");
             background-size: 100% 100%;
             background-repeat: no-repeat;
             height: 32rpx;
-            .font_5 {
+            .tag-text {
+			  margin: 0 8rpx;
               font-size: 20rpx;
               font-family: PingFangSC-Regular;
               line-height: 20rpx;
               color: #bb3e0c;
             }
-            .text_14 {
-              margin: 0 8rpx;
-            }
           }
         }
         .shop-con-bottom {
           margin-top: 40rpx;
-          .font_6 {
+          .font-con_01 {
             font-size: 20rpx;
             font-family: 苹方;
             line-height: 20rpx;
             color: #a7a7a7;
           }
-          .text_16 {
+          .deposit-text {
             color: #bb3e0c;
           }
-          .text_17 {
+          .price-symbol {
             color: #bb3e0c;
             font-size: 20rpx;
             font-family: PingFang SC;
             line-height: 20rpx;
           }
-          .font_7 {
+          .price-number {
+            margin-left: -8rpx;
+            color: #bb3e0c;
             font-size: 32rpx;
             font-family: HarmonyOSSans;
             letter-spacing: -1.24rpx;
             line-height: 32rpx;
             font-weight: 700;
-            color: #030305;
           }
-          .text_18 {
-            margin-left: -8rpx;
-            color: #bb3e0c;
-          }
-          .font_8 {
+          .decimal-font {
             font-size: 20rpx;
             font-family: HarmonyOSSansSC;
             line-height: 20rpx;
             font-weight: 700;
-            color: #030305;
-          }
-          .text_19 {
             color: #bb3e0c;
           }
-          .text_20 {
+          .pre-book {
             margin-left: 32rpx;
             font-size: 22rpx;
             line-height: 22rpx;
