88529edaa9fdfcfbc31a24032c6f4cb77269b52e
garyxdh <garyxdh@gmail.com>
2023-11-28 16:24:55 +0800

    feat: 优化 grid panel，绑定数据

--- components/GridPanel.vue
+++ components/GridPanel.vue
@@ -5,7 +5,12 @@
       <text class="ml-4 title-text">探索家具灵感</text>
     </view>
     <view class="mt-12 flex-col">
-      <swiper class="nav-swiper" style="height: 184rpx" autoplay="true" indicator-dots="true">
+      <swiper
+        class="nav-swiper"
+        style="height: 184rpx"
+        autoplay="true"
+        indicator-dots="true"
+      >
         <swiper-item v-for="(item, index) in swiperItems" :key="index">
           <view class="flex-row items-center equal-division">
             <view
@@ -13,11 +18,8 @@
               v-for="(item, index) in items"
               :key="index"
             >
-              <image
-                class="item-image"
-                src="https://dev.ft.velosoft.cn/api/image?token=6564c3e6cc0204001241016e&name=6087d03087f61aefa21604e8a8260437.png"
-              />
-              <text class="item-text">客厅收纳</text>
+              <image class="item-image" :src="item.icon" />
+              <text class="item-text">{{ item.text }}</text>
             </view>
           </view>
         </swiper-item>
@@ -33,7 +35,28 @@ export default {
   data() {
     return {
       swiperItems: [null, null],
-      items: [null, null, null, null, null],
+      items: [
+        {
+          icon: "https://dev.ft.velosoft.cn/api/image?token=6564c3e6cc0204001241016e&name=6087d03087f61aefa21604e8a8260437.png",
+          text: "客厅收纳",
+        },
+        {
+          icon: "https://dev.ft.velosoft.cn/api/image?token=6564c3e6cc0204001241016e&name=61bc2017b76d4955d49230334853d186.png",
+          text: "衣柜收纳",
+        },
+        {
+          icon: "https://dev.ft.velosoft.cn/api/image?token=6564c3e6cc0204001241016e&name=3824feb130eaa18a0145cbe83a7076df.png",
+          text: "厨房收纳",
+        },
+        {
+          icon: "https://dev.ft.velosoft.cn/api/image?token=6564c3e6cc0204001241016e&name=b5dd0a42660421740bd5ff8e89a49f51.png",
+          text: "卧室收纳",
+        },
+        {
+          icon: "https://dev.ft.velosoft.cn/api/image?token=6564c3e6cc0204001241016e&name=e20217164f231e0999467a2457261afb.png",
+          text: "杂物房收纳",
+        },
+      ],
     };
   },
 
