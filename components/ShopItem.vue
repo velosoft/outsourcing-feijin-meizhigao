80b7571d3c70885b588882581e584fd5ed4db139
gxfan <gxfan@velosoft.cn>
2023-11-28 12:07:04 +0800

    feat: 添加服务中心

--- components/ShopItem.vue
+++ components/ShopItem.vue
@@ -2,26 +2,28 @@
   <view class="flex-col shop-item">
     <image
       class="a-i-background-image shop-a-i-background-image"
-      src="https://dev.ft.velosoft.cn/api/image?token=6564447bcc0204001240f5d1&name=69f26c588c4040c0a2ae6b1d6c407546.png"
+      :src="image"
     />
-    <text class="self-start shop-text">厨房定制收纳服务</text>
+    <text class="self-start shop-title">{{ title }}</text>
     <view class="flex-row self-stretch shop-equal-division">
-      <view class="flex-col items-center shop-text-wrapper">
-        <text class="shop-CenterPannel-font shop-text_1">新品上市</text>
-      </view>
-      <view class="flex-col items-center shop-text-wrapper_1 shop-ml-5">
-        <text class="shop-CenterPannel-font shop-text_1">好评推荐</text>
-      </view>
-      <view class="flex-col items-center shop-text-wrapper shop-view shop-ml-5">
-        <text class="shop-CenterPannel-font shop-text_1">新客优选</text>
+      <view
+        class="flex-col items-center shop-text-wrapper"
+        v-for="(item, index) in tags"
+        :key="index"
+      >
+        <text class="shop-CenterPannel-font shop-text_1">{{ item }}</text>
       </view>
     </view>
-    <view class="flex-row self-stretch relative shop-group">
-      <text class="self-start shop-CenterPannel-font">秒杀价</text>
-      <text class="self-center shop-text_2">200</text>
-      <text class="shop-text_3 shop-pos">￥</text>
-      <text class="self-start relative shop-text_4">.00</text>
-      <text class="self-center relative shop-text_5">100人购买</text>
+    <view class="flex-row justify-between items-center self-stretch shop-group">
+      <view class="flex-row items-center self-stretch shop-price-wrapper">
+        <text class="shop-CenterPannel-font">秒杀价</text>
+        <view class="flex-row items-center shrink-0 relative group_4 ml-1">
+          <text class="shop-text_3">￥</text>
+          <text class="shop-text_2 pos">{{ price }}</text>
+          <text class="relative shop-text_4">.00</text>
+        </view>
+      </view>
+      <text class="shop-text_5">{{ buyers + "人购买" }}</text>
     </view>
   </view>
 </template>
@@ -29,7 +31,13 @@
 <script>
 export default {
   components: {},
-  props: {},
+  props: {
+    image: { type: String, default: `` },
+    title: { type: String, default: `` },
+    tags: { type: Array, default: () => [] },
+    price: { type: Number, default: 0 },
+    buyers: { type: Number, default: 0 },
+  },
   data() {
     return {};
   },
@@ -38,101 +46,95 @@ export default {
 };
 </script>
 
-<style scoped lang="less">
+<style scoped lang="css">
+.ml-1 {
+  margin-left: 2rpx;
+}
 .shop-item {
   padding-bottom: 34.48rpx;
-  background-image: url("https://dev.ft.velosoft.cn/api/image?token=6564447bcc0204001240f5d1&name=1fe6dd1ef410d3f49fa1ec58d8026298.png");
+  background-image: url("https://project-user-resource-1256085488.cos.ap-guangzhou.myqcloud.com/5f994f8347e00b001139c3d4/6564419acc0204001240f4ec/17010692422784410573.png");
   background-size: 100% 100%;
   background-repeat: no-repeat;
-  .a-i-background-image {
-    flex-shrink: 0;
-    align-self: stretch;
-  }
-  .shop-a-i-background-image {
-    width: 330rpx;
-    height: 330rpx;
-  }
-  .shop-text {
-    margin-left: 11rpx;
-    margin-top: 30rpx;
-    color: #111111;
-    font-size: 28rpx;
-    font-family: PingFang SC;
-    line-height: 28rpx;
-  }
-  .shop-equal-division {
-    margin-top: 23.26rpx;
-    padding: 0 10rpx;
-    .shop-text-wrapper {
-      flex: 1 1 96rpx;
-      padding: 3.44rpx 0 5.96rpx;
-      background-color: #fff1ed;
-      border-radius: 4rpx;
-      height: 28rpx;
-    }
-    .shop-text-wrapper_1 {
-      padding: 3.44rpx 0 5.98rpx;
-      flex: 1 1 96rpx;
-      background-color: #fff1ed;
-      border-radius: 4rpx;
-      height: 28rpx;
-    }
-    .shop-ml-5 {
-      margin-left: 10rpx;
-    }
-    .shop-text_1 {
-      text-transform: uppercase;
-    }
-    .shop-view {
-      margin-right: 2rpx;
-    }
-  }
-  .shop-group {
-    margin-top: 33.74rpx;
-    padding-left: 10.78rpx;
-    padding-right: 1.16rpx;
-    .shop-text_2 {
-      margin-left: 14.6rpx;
-      color: #bb3e0c;
-      font-size: 30rpx;
-      font-family: HarmonyOSSans;
-      font-weight: 700;
-      line-height: 30rpx;
-      letter-spacing: -1.16rpx;
-    }
-    .shop-text_3 {
-      color: #bb3e0c;
-      font-size: 20rpx;
-      font-family: PingFang SC;
-      line-height: 20rpx;
-    }
-    .shop-pos {
-      position: absolute;
-      left: 74.22rpx;
-      top: 0;
-    }
-    .shop-text_4 {
-      margin-left: 1.6rpx;
-      margin-top: 1.34rpx;
-      color: #bb3e0c;
-      font-size: 20rpx;
-      font-family: HarmonyOSSansSC;
-      font-weight: 700;
-      line-height: 20rpx;
-    }
-    .shop-text_5 {
-      margin-left: 39.86rpx;
-      color: #a7a7a7;
-      font-size: 24rpx;
-      font-family: 苹方;
-      line-height: 24rpx;
-    }
-  }
-  .shop-CenterPannel-font {
-    font-size: 20rpx;
-    font-family: 苹方;
-    line-height: 20rpx;
-    color: #bb3e0c;
-  }
+}
+.a-i-background-image {
+  flex-shrink: 0;
+  align-self: stretch;
+}
+.shop-a-i-background-image {
+  width: 330rpx;
+  height: 330rpx;
+}
+.shop-title {
+  margin-left: 11rpx;
+  margin-top: 30rpx;
+  color: #111111;
+  font-size: 28rpx;
+  font-family: PingFang SC;
+  line-height: 28rpx;
+}
+.shop-equal-division {
+  margin-top: 23.26rpx;
+  padding: 0 10rpx;
+}
+.shop-text-wrapper {
+  flex: 1 1 96rpx;
+  padding: 3.44rpx 0 5.96rpx;
+  background-color: #fff1ed;
+  border-radius: 4rpx;
+  height: 28rpx;
+}
+.shop-CenterPannel-font {
+  font-size: 20rpx;
+  font-family: 苹方;
+  line-height: 20rpx;
+  color: #bb3e0c;
+}
+.shop-text_1 {
+  text-transform: uppercase;
+}
+.shop-group {
+  margin-top: 33.74rpx;
+  padding-left: 10.78rpx;
+  padding-right: 1.16rpx;
+}
+.shop-price-wrapper {
+  margin-bottom: 2rpx;
+  padding-bottom: 5rpx;
+}
+.group_4 {
+  padding-bottom: 6.9rpx;
+}
+.shop-text_3 {
+  color: #bb3e0c;
+  font-size: 20rpx;
+  font-family: PingFang SC;
+  line-height: 20rpx;
+}
+.shop-text_2 {
+  color: #bb3e0c;
+  font-size: 30rpx;
+  font-family: HarmonyOSSans;
+  font-weight: 700;
+  line-height: 30rpx;
+  letter-spacing: -1.16rpx;
+}
+.pos {
+  position: absolute;
+  left: 13.16rpx;
+  bottom: 0;
+}
+.shop-text_4 {
+  margin-left: 44.76rpx;
+  color: #bb3e0c;
+  font-size: 20rpx;
+  font-family: HarmonyOSSansSC;
+  font-weight: 700;
+  line-height: 20rpx;
+}
+.shop-text_5 {
+  color: #a7a7a7;
+  font-size: 24rpx;
+  font-family: 苹方;
+  line-height: 24rpx;
 }
 </style>
--- pages/tabBar/personal.vue
+++ pages/tabBar/personal.vue
@@ -1,22 +1,238 @@
 <template>
-	<view>
-		
-	</view>
+  <view class="flex-col page">
+    <view class="flex-col flex-1 body">
+      <view class="flex-col justify-start carousel">
+        <view
+          class="flex-col justify-start items-center a-i-background-image-wrapper_1"
+        >
+          <image
+            class="a-i-background-image"
+            src="https://dev.ft.velosoft.cn/api/image?token=6564c3e6cc0204001241016e&name=9e679835f4a2e239875a79bfc773b941.png"
+          />
+        </view>
+      </view>
+      <view class="flex-col">
+        <u-tabs
+          class="tabs-header"
+          lineColor="#b09053"
+          lineWidth="52rpx"
+          lineHeight="4rpx"
+          :activeStyle="{
+            color: '#030305',
+            fontSize: '24rpx',
+            fontFamily: 'PingFang SC',
+            lineHeight: '24rpx',
+          }"
+          :inactiveStyle="{
+            fontSize: '24rpx',
+            fontFamily: '苹方',
+            lineHeight: '24rpx',
+            color: '#6c6c6c',
+          }"
+          :list="list"
+        ></u-tabs>
+        <view class="mt-12 flex-row tabs_content">
+          <view class="flex-col self-start relative group_6">
+            <ShopItem
+              class="mt-12 list-item"
+              v-for="(item, index) in items"
+              :key="index"
+              :image="item.image"
+              :title="item.title"
+              :tags="item.tags"
+              :price="item.price"
+              :buyer="item.buyer"
+            ></ShopItem>
+          </view>
+          <view class="ml-16 flex-col group_7">
+            <ShopItem
+              class="mt-12 list-item"
+              v-for="(item, index) in items_1"
+              :key="index"
+              :image="item.image"
+              :title="item.title"
+              :tags="item.tags"
+              :price="item.price"
+              :buyer="item.buyer"
+            ></ShopItem>
+          </view>
+        </view>
+      </view>
+      <view class="flex-col justify-start items-center loading-wrapper">
+        <text class="font_10 text_35">加载中~</text>
+      </view>
+    </view>
+  </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				
-			}
-		},
-		methods: {
-			
-		}
-	}
+import ShopItem from "../../components/ShopItem.vue";
+
+export default {
+  components: {
+    ShopItem,
+  },
+  props: {},
+  data() {
+    return {
+      list: [
+        {
+          name: "全部",
+        },
+        {
+          name: "单区收纳",
+        },
+        {
+          name: "全屋收纳",
+        },
+        {
+          name: "卧室收纳",
+        },
+        {
+          name: "衣柜收纳",
+        },
+      ],
+      items: [
+        {
+          image:
+            "https://project-user-resource-1256085488.cos.ap-guangzhou.myqcloud.com/5f994f8347e00b001139c3d4/6564419acc0204001240f4ec/17010692430820111285.png",
+          title: "厨房定制收纳服务",
+          tags: ["新品上市", "好评推荐", "新客优选"],
+          price: 200,
+          buyers: 100,
+        },
+        {
+          image:
+            "https://project-user-resource-1256085488.cos.ap-guangzhou.myqcloud.com/5f994f8347e00b001139c3d4/6564419acc0204001240f4ec/17010692430820111285.png",
+          title: "厨房定制收纳服务",
+          tags: ["新品上市", "好评推荐", "新客优选"],
+          price: 200,
+          buyers: 100,
+        },
+        {
+          image:
+            "https://project-user-resource-1256085488.cos.ap-guangzhou.myqcloud.com/5f994f8347e00b001139c3d4/6564419acc0204001240f4ec/17010692430820111285.png",
+          title: "厨房定制收纳服务",
+          tags: ["新品上市", "好评推荐", "新客优选"],
+          price: 200,
+          buyers: 100,
+        },
+
+        {
+          image:
+            "https://project-user-resource-1256085488.cos.ap-guangzhou.myqcloud.com/5f994f8347e00b001139c3d4/6564419acc0204001240f4ec/17010692430820111285.png",
+          title: "厨房定制收纳服务",
+          tags: ["新品上市", "好评推荐", "新客优选"],
+          price: 200,
+          buyers: 100,
+        },
+      ],
+      items_1: [
+        {
+          image:
+            "https://project-user-resource-1256085488.cos.ap-guangzhou.myqcloud.com/5f994f8347e00b001139c3d4/6564419acc0204001240f4ec/17010692430820111285.png",
+          title: "厨房定制收纳服务",
+          tags: ["新品上市", "好评推荐", "新客优选"],
+          price: 200,
+          buyers: 100,
+        },
+        {
+          image:
+            "https://project-user-resource-1256085488.cos.ap-guangzhou.myqcloud.com/5f994f8347e00b001139c3d4/6564419acc0204001240f4ec/17010692430820111285.png",
+          title: "厨房定制收纳服务",
+          tags: ["新品上市", "好评推荐", "新客优选"],
+          price: 200,
+          buyers: 100,
+        },
+        {
+          image:
+            "https://project-user-resource-1256085488.cos.ap-guangzhou.myqcloud.com/5f994f8347e00b001139c3d4/6564419acc0204001240f4ec/17010692430820111285.png",
+          title: "厨房定制收纳服务",
+          tags: ["新品上市", "好评推荐", "新客优选"],
+          price: 200,
+          buyers: 100,
+        },
+
+        {
+          image:
+            "https://project-user-resource-1256085488.cos.ap-guangzhou.myqcloud.com/5f994f8347e00b001139c3d4/6564419acc0204001240f4ec/17010692430820111285.png",
+          title: "厨房定制收纳服务",
+          tags: ["新品上市", "好评推荐", "新客优选"],
+          price: 200,
+          buyers: 100,
+        },
+      ],
+    };
+  },
+
+  methods: {},
+};
 </script>
 
-<style lang="less" scoped>
+<style scoped lang="css">
+.page {
+  background-color: #f8f8f8;
+  width: 100%;
+  overflow-y: auto;
+  overflow-x: hidden;
+  height: 100%;
+}
 
+.body {
+  padding-bottom: 8rpx;
+  overflow-y: auto;
+}
+
+.carousel {
+  background-color: #ffffff;
+}
+
+.a-i-background-image-wrapper_1 {
+  margin: 0 32rpx;
+}
+
+.a-i-background-image {
+  width: 92vw;
+  height: 26.4vw;
+}
+
+.tabs-header {
+  background-color: #ffffff;
+}
+
+.tabs_content {
+  padding: 0 32rpx;
+}
+
+.group_6 {
+  flex: 1 1 330rpx;
+}
+
+.list-item {
+  position: relative;
+}
+
+.list-item:first-child {
+  margin-top: 0;
+}
+
+.font_10 {
+  font-size: 24rpx;
+  font-family: 苹方;
+  line-height: 24rpx;
+  color: #a7a7a7;
+}
+
+.group_7 {
+  flex: 1 1 330rpx;
+}
+
+.loading-wrapper {
+  padding: 40rpx 0;
+}
+
+.text_35 {
+  font-size: 26rpx;
+  line-height: 26rpx;
+}
 </style>
