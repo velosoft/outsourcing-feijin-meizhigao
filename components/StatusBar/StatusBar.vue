9250e6920762f07505362b89f71a7c006e86a1d5
Ronin <42492283+joshuabsyxzry@users.noreply.github.com>
2023-12-07 18:07:37 +0800

    feat:修改navbar组件

---
+++ components/StatusBar/StatusBar.vue
@@ -0,0 +1,22 @@
+<template>
+	<view :style="{ height: statusBarHeight + 'px' }" class="statusBarComp">
+		<slot />
+	</view>
+</template>
+
+<script>
+export default {
+  name: 'StatusBarComp',
+  data () {
+    return {
+      statusBarHeight: 20
+    };
+  },
+  mounted () {
+    this.statusBarHeight = uni.getSystemInfoSync().statusBarHeight;
+  }
+};
+</script>
+
+<style lang="scss" scoped >
+</style>
