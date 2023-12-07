841d603d379c19ec6713ab1bf0a95b9878f4c508
Ronin <42492283+joshuabsyxzry@users.noreply.github.com>
2023-12-07 13:16:31 +0800

    feat:修改首页

--- components/ClassifyCards.vue
+++ components/ClassifyCards.vue
@@ -1,12 +1,35 @@
 <template>
   <view class="flex-row">
-    <view class="flex-col items-center classify-card" @click="goto">
-      <image class="classify-bg" src="../static/images/bg_shop_01.png" />
-      <text class="classify-title classify-pos">服务分类</text>
+    <view
+      class="flex-row items-center classify-card classify-card-left"
+      @click="goto"
+    >
+      <view class="classify-con">
+        <view class="tag tag-left">
+          <text class="tag-text">秒杀活动</text>
+        </view>
+        <text class="tag-down">让你购物省心又省钱</text>
+      </view>
+      <image
+        class="classify-bg-left"
+        src="../static/images/classify_left.png"
+      />
     </view>
-    <view class="ml-12 flex-col items-center classify-card">
-      <image class="classify-bg" src="../static/images/bg_service_01.png" />
-      <text class="classify-title classify-pos">商品分类</text>
+
+    <view
+      class="flex-row items-center ml-8 classify-card classify-card-right"
+      @click="goto"
+    >
+      <view class="classify-con">
+        <view class="tag tag-right">
+          <text class="tag-text">领券中心</text>
+        </view>
+        <text class="tag-down">领券下单更优惠</text>
+      </view>
+      <image
+        class="classify-bg-right"
+        src="../static/images/classify_right.png"
+      />
     </view>
   </view>
 </template>
@@ -29,24 +52,63 @@ export default {
 };
 </script>
 
-<style scoped lang="css">
+<style scoped lang="less">
 .classify-card {
-  position: relative;
-  flex: 1 1 332rpx;
-}
-.classify-bg {
   width: 332rpx;
-  height: 176rpx;
+  height: 150rpx;
+  position: relative;
+  padding: 24rpx;
+  border-radius: 3px;
+
+  &-left {
+    background: rgba(193, 179, 172, 0.4);
+  }
+  &-right {
+    background: rgba(239, 227, 221, 0.4);
+  }
+  .classify-bg-left {
+    position: absolute;
+    bottom:8rpx;
+    right: 16rpx;
+    width: 100rpx;
+    height: 100rpx;
+  }
+  .classify-bg-right {
+    position: absolute;
+    bottom: 4rpx;
+    right: 16rpx;
+    width: 130rpx;
+    height: 130rpx;
+  }
 }
-.classify-title {
-  font-size: 26rpx;
-  line-height: 26rpx;
-  font-family: PingFangSC-Medium;
-  color: #ffffff;
-}
-.classify-pos {
-  position: absolute;
-  left: 26rpx;
-  top: 30rpx;
+.classify-con {
+  .tag {
+    width: 116rpx;
+    height: 40rpx;
+
+    padding: 0 10rpx;
+
+    &-left {
+      background: #b09053ff;
+    }
+
+    &-right {
+      background-color: #bb3e0c;
+    }
+    .tag-text {
+      font-family: PingFangSC;
+      font-size: 24rpx;
+      font-weight: 500;
+      line-height: 36rpx;
+      color: rgba(255, 255, 255, 1);
+    }
+  }
+  .tag-down {
+    font-family: 苹方;
+    font-size: 20rpx;
+    font-weight: 400;
+    line-height: 28rpx;
+    color: rgba(3, 3, 5, 1);
+  }
 }
 </style>
