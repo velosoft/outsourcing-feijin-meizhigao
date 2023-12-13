9401065f0549915c0d39232d9d66b48bd8d738d7
dannnney <dannnney@gmail.com>
2023-12-13 18:59:17 +0800

    增加黑色 button 样式覆盖

--- pages/showcase/button.vue
+++ pages/showcase/button.vue
@@ -36,6 +36,24 @@
       ></u-button>
       <u-button class="button ml-10" text="预约服务" type="primary"></u-button>
     </view>
+
+    <view class="flex-col mt-20">
+      <text>黑色按钮</text>
+      <view class="flex-row mt-10">
+        <u-button
+          plain
+          text="取消订单"
+          size="small"
+          class="cf-black-btn"
+        ></u-button>
+        <u-button
+          type="primary"
+          text="去服务"
+          size="small"
+          class="cf-black-btn ml-20"
+        ></u-button>
+      </view>
+    </view>
   </view>
 </template>
 
@@ -67,5 +85,30 @@ export default {
 }
 .button {
   flex: 1 1 0;
+}
+
+.cf-black-btn {
+  /deep/ .u-button {
+    border-radius: 4px;
+    font-weight: 700;
+  }
+
+  /deep/ .u-button--primary {
+    background-color: #000000 !important;
+    border-color: #000000 !important;
+  }
+
+  /deep/ .u-button--small {
+    min-width: 132rpx;
+    height: 52rpx;
+  }
+
+  /deep/ .u-button--info {
+    border-color: solid 2rpx #cecece !important;
+  }
+
+  /deep/ .u-button--plain.u-button--info {
+    color: #9e9ea0;
+  }
 }
 </style>
