b8953187b8df8d7a41ad9693c4b018a0b4e4bcc2
garyxdh <garyxdh@gmail.com>
2023-12-16 00:55:49 +0800

    feat: 绑定规格选择 popup 中的数据

--- pages/detailPages/popup/SpecificationPopup.vue
+++ pages/detailPages/popup/SpecificationPopup.vue
@@ -8,33 +8,59 @@
         />
         <view class="flex-col flex-1 product-right ml-12">
           <view class="flex-col">
-            <view class="flex-row items-center self-stretch">
-              <text class="com-font price-label-text">秒杀价</text>
-              <text class="price-symbol price-symbol-text">￥</text>
-              <text class="price">200</text>
-              <text class="price">.00</text>
+            <view
+              :class="
+                'flex-row items-center self-stretch' +
+                (productSpec.isActivity ? ' activity-color' : 'normal-color')
+              "
+            >
+              <text
+                v-if="productSpec.isActivity"
+                class="com-font activity-color"
+                >秒杀价</text
+              >
+              <text class="price-symbol">￥</text>
+              <text class="price">{{ productSpec.price }}</text>
+              <text class="price">.{{ productSpec.decimalPrice }}</text>
             </view>
             <text class="self-start com-font stock-text mt-16"
-              >库存：999件</text
+              >库存：{{ productSpec.invertory }}件</text
             >
           </view>
-          <text class="com-font speci-text mt-24"
-            >请选择规格 / 已选：3层80长*40宽*170高</text
-          >
+          <text class="com-font speci-text mt-24">{{ lastChoiceText }}</text>
         </view>
       </view>
-      <view class="flex-col spec-select">
-        <text class="self-start spec-font spec-text">请选择规格</text>
-        <selector
-          class="variant-selector service-varaint mt-18"
-          :items="timelineItems"
-          @change="onChange"
-        ></selector>
-      </view>
-      <view class="flex-row justify-between buy-number">
-        <text class="self-start price-symbol buy-number-label">购买数量</text>
-        <u-number-box v-model="count"></u-number-box>
-      </view>
+
+      <scroll-view>
+        <view
+          class="flex-col spec-select"
+          v-for="(item, idx) of productSpec.specs"
+          :key="idx"
+        >
+          <text class="self-start spec-font spec-text">{{ item.title }}</text>
+          <selector
+            class="variant-selector service-varaint mt-18"
+            :items="item.options"
+            @change="(v) => onChange(idx, v)"
+          ></selector>
+        </view>
+        <view class="flex-row justify-between buy-number">
+          <text class="self-start price-symbol buy-number-label">购买数量</text>
+          <u-number-box v-model="count"></u-number-box>
+        </view>
+        <view
+          class="flex-col spec-select"
+          v-for="(item, idx) of productSpec.specialSpecs"
+          :key="idx"
+        >
+          <text class="self-start spec-font spec-text">{{ item.title }}</text>
+          <selector
+            class="variant-selector service-varaint mt-18"
+            :items="item.options"
+            @change="(v) => onSpecialChange(idx, v)"
+          ></selector>
+        </view>
+      </scroll-view>
     </view>
     <u-button
       class="mt-54"
@@ -56,26 +82,67 @@ export default {
       default: () => ({
         imgUrl: "",
         price: 100,
-        stock: 100,
-        tag: ["apple", "orange", "banana"],
+        decimalPrice: 100,
+        invertory: 999,
+        isActivity: true,
+        specs: [
+          // {
+          //   title: "请选择规格",
+          //   options: [
+          //     "3层60长*40宽*170高可加高加层",
+          //     "3层80长*40宽*170高可加高加层",
+          //     "3层80长*40宽*170高可加高加层",
+          //   ],
+          // },
+          // {
+          //   title: "尺寸",
+          //   options: ["小号", "中号", "大号", "特大"],
+          // },
+          {
+            title: "类型",
+            options: [
+              "按延米核算（元/m)",
+              "按面积核算（元/m)",
+              "按小时核算（元/人/h)",
+            ],
+          },
+          {
+            title: "颜色",
+            options: ["黄色", "红色", "黑色", "蓝色"],
+          },
+        ],
+        specialSpecs: [
+          // {
+          //   title: "附加服务",
+          //   options: ["安装服务  ￥88.00", "安装服务  ￥99.00"],
+          // },
+        ],
       }),
     },
   },
   data() {
     return {
-      timelineItems: [
-        "3层60长*40宽*170高可加高加层",
-        "3层80长*40宽*170高可加高加层",
-        "3层80长*40宽*170高可加高加层",
-      ],
       count: 2,
+      userChoice: {},
+      lastChoiceText: "",
     };
   },
-
+  mounted() {
+    if (this.productSpec.specs.length) {
+      this.lastChoiceText = "请选择规格";
+    }
+  },
   methods: {
-    onChange(index) {
-      // 事件处理方法
-      console.log("outter:" + index);
+    onChange(specIdx, optionIdx) {
+      let spec = this.productSpec.specs[specIdx];
+      this.lastChoiceText = "已选：" + spec.options[optionIdx];
+      this.userChoice[spec.title] = spec.options[optionIdx];
+    },
+    onSpecialChange(specIdx, optionIdx) {
+      let spec = this.productSpec.specialSpecs[specIdx];
+      this.lastChoiceText =
+        "已选：" + this.productSpec.specialSpecs[specIdx].options[optionIdx];
+      this.userChoice[spec.title] = spec.options[optionIdx];
     },
     goNext() {
       this.$emit("goNext");
@@ -101,10 +168,10 @@ export default {
       line-height: 26rpx;
       color: #6c6c6c;
     }
-    .price-label-text {
-      color: #bb3e0c;
+    .normal-color {
+      color: #030305;
     }
-    .price-symbol-text {
+    .activity-color {
       color: #bb3e0c;
     }
     .price {
@@ -112,7 +179,6 @@ export default {
       font-family: HarmonyOS Sans SC;
       line-height: 32rpx;
       font-weight: 700;
-      color: #bb3e0c;
     }
     .stock-text {
       font-size: 28rpx;
