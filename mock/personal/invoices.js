bae5daa359fd51334e66072dbe522879960a895e
garyxdh <garyxdh@gmail.com>
2023-12-18 17:06:37 +0800

    feat: 补充发票抬头列表 mock 数据

---
+++ mock/personal/invoices.js
@@ -0,0 +1,53 @@
+// 发票抬头列表
+export const invoiceHeaders = [
+  {
+    title: "广州飞进信息科技有限公司",
+    type: "增值税专用发票-企业抬头",
+    isDefault: true,
+  },
+  {
+    title: "深圳云动科技有限公司",
+    type: "增值税普通发票-企业抬头",
+    isDefault: false,
+  },
+  {
+    title: "北京乐视科技有限公司",
+    type: "普通发票-企业抬头",
+    isDefault: false,
+  },
+  {
+    title: "上海华为通信技术有限公司",
+    type: "增值税专用发票-企业抬头",
+    isDefault: true,
+  },
+  {
+    title: "深圳腾讯科技有限公司",
+    type: "增值税普通发票-企业抬头",
+    isDefault: false,
+  },
+  {
+    title: "杭州阿里巴巴集团有限公司",
+    type: "增值税专用发票-企业抬头",
+    isDefault: true,
+  },
+  {
+    title: "南京苏宁易购电子商务有限公司",
+    type: "普通发票-企业抬头",
+    isDefault: false,
+  },
+  {
+    title: "广州小米科技有限责任公司",
+    type: "增值税专用发票-企业抬头",
+    isDefault: true,
+  },
+  {
+    title: "成都美团点评科技有限公司",
+    type: "增值税普通发票-企业抬头",
+    isDefault: false,
+  },
+  {
+    title: "武汉京东世纪贸易有限公司",
+    type: "普通发票-企业抬头",
+    isDefault: false,
+  },
+];
