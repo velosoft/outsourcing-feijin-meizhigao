b20a4309d8737289116f75cb666304b4cf2b17f0
dannnney <dannnney@gmail.com>
2023-12-20 16:36:01 +0800

    新增 ListWithEmpty 组件

---
+++ components/ListContainer/ListWithEmpty.vue
@@ -0,0 +1,64 @@
+<template>
+  <view class="listContainerComp">
+    <view class="emptyWrapper" v-if="showEmpty">
+      <slot name="customEmpty">
+        <empty
+          class="flex-1"
+          width="408"
+          height="286"
+          :imgSrc="imgSrc"
+          :hint="emptyHint"
+          :paddingTop="emptyPaddingTop"
+        />
+      </slot>
+    </view>
+    <template v-else>
+      <slot />
+      <Loading :finished="finished" v-if="showLoading" />
+    </template>
+  </view>
+</template>
+
+<script>
+  import Empty from '../Empty/Empty.vue';
+  import Loading from '../Loading/Loading.vue';
+
+  export default {
+    name: 'ListContainer',
+    components: {
+      Empty,
+      Loading,
+    },
+    props: {
+      showEmpty: {
+        type: Boolean,
+        default: false,
+      },
+      showLoading: {
+        type: Boolean,
+        default: true,
+      },
+      finished: {
+        type: Boolean,
+        default: false,
+      },
+      imgSrc: {
+        type: String,
+        default: '/static/images/icon_empty.png',
+      },
+      emptyPaddingTop: {
+        type: Number,
+        default: 0, // 单位rpx
+      },
+      emptyHint: {
+        type: String,
+        default: '暂无内容~',
+      },
+    },
+    data() {
+      return {};
+    },
+  };
+</script>
+
+<style lang="less" scoped></style>
