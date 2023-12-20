ddf016319a35f8a9d546fb512e2a68acfaeee401
garyxdh <garyxdh@gmail.com>
2023-12-20 16:59:31 +0800

    feat: 优化未使用状态优惠券的 list item 样式和布局

--- pages/personal/coupon/components/UnusedCoupon/UnusedCoupon.vue
+++ pages/personal/coupon/components/UnusedCoupon/UnusedCoupon.vue
@@ -1,34 +1,32 @@
 <template>
-  <view class="flex-col justify-start relative">
+  <view class="flex-col justify-start relative coupon">
     <image class="coup_bg pos" src="/static/images/bg_coupon_unused.png" />
-    <view class="flex-row items-center relative">
+    <view class="flex-row items-stretch relative coupon">
       <view class="flex-col shrink-0 price-wrap items-center">
         <view class="flex-row items-baseline price-num">
-          <text class="price">{{item.amount}}</text>
-          <text class="currency currency_text ml-4">元</text>
+          <text class="price">{{ item.amount }}</text>
+          <text class="font-base currency_text ml-4">元</text>
         </view>
-        <text class="font mt-8">{{"满" + item.triggerCondition +"元使用"}}</text>
+        <text class="font mt-6">{{ '满' + item.triggerCondition + '元使用' }}</text>
       </view>
       <view class="shrink-0 line"></view>
-      <view class="flex-row justify-center items-center flex-1 coupon-right">
-        <view class="flex-col">
-          <view class="flex-col items-start self-stretch">
-            <text class="currency title">{{item.title}}</text>
-            <view class="flex-col text-wrapper mt-8 items-center">
-              <text class="coupon-time coupon-scope">{{item.applicableTo}}</text>
+      <view class="flex-col justify-between items-stretch flex-1 coupon-right">
+        <view class="flex-col jusfity-start align-stretch">
+          <text class="font-base title line-clamp-one">{{ item.title }}</text>
+          <view class="flex-row justify-between mt-8">
+            <view class="flex-col justify-center items-center self-start text-wrapper">
+              <text class="coupon-time coupon-scope">{{ item.applicableTo }}</text>
             </view>
+            <view class="coupon-btn">立即使用</view>
           </view>
-          <text class="self-start coupon-time mt-20">{{item.validity.start + "-" + item.validity.end}}</text>
         </view>
-        <view class="flex-col justify-start btn-wrap ml-16">
-          <u-button class="coupon-btn" text="立即使用" type="primary" size="mini" shape="circle"></u-button>
-        </view>
+        <text class="self-start coupon-time">{{ item.validity.start + '-' + item.validity.end }}</text>
       </view>
     </view>
   </view>
-  </template>
-  
-  <script>
+</template>
+
+<script>
   export default {
     components: {},
     props: {
@@ -47,12 +45,15 @@
     data() {
       return {};
     },
-  
+
     methods: {},
   };
-  </script>
-  
-  <style scoped lang="less">
+</script>
+
+<style scoped lang="less">
+  .coupon {
+    height: 230rpx;
+  }
   .coup_bg {
     width: 702rpx;
     height: 230rpx;
@@ -65,7 +66,7 @@
     bottom: 0;
   }
   .price-wrap {
-    padding: 32rpx 36rpx 48rpx;
+    padding: 50rpx 36rpx 50rpx;
     width: 256rpx;
     .price-num {
       margin-right: 8rpx;
@@ -73,8 +74,8 @@
       .price {
         color: #ff4d1d;
         font-size: 76rpx;
-        font-family: '.AppleSystemUIFont';
-        line-height: 90rpx;
+        font-family: 'HarmonyOS Sans Bold';
+        line-height: 76rpx;
       }
       .currency_text {
         color: #ff4d1d;
@@ -88,24 +89,19 @@
     }
   }
   .line {
-    background-image: repeating-linear-gradient(
-      0deg,
-      #e8e8e8,
-      #e8e8e8 3.4090909090909087%,
-      transparent 3.4090909090909087%,
-      transparent 6.8181818181818175%
-    );
+    background-image: repeating-linear-gradient(0deg, #e8e8e8, #e8e8e8 3.4%, transparent 3.4%, transparent 6.8%);
     width: 2rpx;
     height: 176rpx;
   }
   .coupon-right {
-    padding: 32rpx 0 32rpx 8rpx;
+    padding: 36rpx 24rpx 32rpx 34rpx;
     .title {
       color: #000000;
       font-weight: 500;
+      line-height: 44rpx;
     }
     .text-wrapper {
-      border-radius: 8rpx;
+      border-radius: 6rpx;
       border: solid 1rpx #9e9ea0;
       .coupon-scope {
         margin: 0 8rpx;
@@ -117,13 +113,21 @@
       line-height: 28rpx;
       color: #8c8f95;
     }
-    .btn-wrap {
-      width: 136rpx;
-    }
   }
-  .currency {
+  .font-base {
     font-size: 32rpx;
     font-family: 'PingFang SC';
-    line-height: 44rpx;
+    line-height: 32rpx;
   }
-  </style>
+  .coupon-btn {
+    padding: 8rpx 20rpx;
+    background-color: #ff4d1d;
+    border-radius: 24rpx;
+
+    color: #ffffff;
+    font-size: 24rpx;
+    font-family: PingFangSC;
+    font-weight: 500;
+    line-height: 34rpx;
+  }
+</style>
