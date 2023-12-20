c8386f305548c2f04e439aff9fb54e5d2b42a6a8
garyxdh <garyxdh@gmail.com>
2023-12-20 18:34:49 +0800

    bugfix: 修复【领券中心】banner 图丢失的 bug

--- pages/coupon/couponCenter.vue
+++ pages/coupon/couponCenter.vue
@@ -1,18 +1,9 @@
 <template>
   <view class="flex-col page">
-    <nav-bar
-      :hasBack="true"
-      :title="title"
-      :fixed="true"
-      :isShow="true"
-      background="#ffffff"
-    ></nav-bar>
+    <nav-bar :hasBack="true" :title="title" :fixed="true" :isShow="true" background="#ffffff"></nav-bar>
     <view class="flex-col flex-1 relative main">
       <view class="flex-col justify-start items-center banner-image-wrapper">
-        <image
-          class="shrink-0 banner-image"
-          src="/static/images/17021047433209292544.png"
-        />
+        <image class="shrink-0 banner-image" src="/static/mock/mock_coupon_banner.png" />
       </view>
       <fj-sticky :customNavHeight="80">
         <u-tabs
@@ -31,19 +22,9 @@
         >
         </u-tabs>
       </fj-sticky>
-      <list-container
-        class="flex-auto mt-12"
-        :showEmpty="!list.length"
-        :showLoading="showLoading"
-        :finished="finished"
-      >
+      <list-container class="flex-auto mt-12" :showEmpty="!list.length" :showLoading="showLoading" :finished="finished">
         <view class="flex-col">
-          <coupon-item
-            class="list-item"
-            v-for="(item, index) in list"
-            :key="index"
-            :coupon="item"
-          ></coupon-item>
+          <coupon-item class="list-item" v-for="(item, index) in list" :key="index" :coupon="item"></coupon-item>
         </view>
       </list-container>
     </view>
@@ -54,125 +35,121 @@
 </template>
 
 <script>
-import CouponItem from "./components/CouponItem.vue";
-import FjSticky from "@/components/FjSticky.vue";
-import ListContainer from "@/components/ListContainer/ListContainer.vue";
-import NavBar from "@/components/NavBar/NavBar.vue";
-import { couponList } from "../../mock/couponList";
+  import CouponItem from './components/CouponItem.vue';
+  import FjSticky from '@/components/FjSticky.vue';
+  import ListContainer from '@/components/ListContainer/ListContainer.vue';
+  import NavBar from '@/components/NavBar/NavBar.vue';
+  import { couponList } from '../../mock/couponList';
 
-export default {
-  components: { CouponItem, FjSticky, ListContainer, NavBar, FjSticky, NavBar },
-  props: {},
-  data() {
-    return {
-      title: "领券中心",
-      tabs: [
-        {
-          name: "全部",
-        },
-        {
-          name: "满减券",
-        },
-        {
-          name: "无门槛优惠券",
-        },
-      ],
-      tab: 0,
-      showLoading: true,
-      finished: false,
-      list: [],
-      page: 1,
-    };
-  },
-  mounted() {
-    setTimeout(() => {
-      this.list = couponList;
-    }, 1500);
-  },
-  onReachBottom() {
-    if (this.page >= 2) {
-      this.finished = true;
-      return;
-    }
-    this.finished = false;
-
-    setTimeout(() => {
-      this.list = this.list.concat(this.list);
-      this.page++;
-    }, 1500);
-  },
-  methods: {
-    onChangeTab(val) {
-      this.tab = val.index;
-      this.list = couponList;
-      this.page = 1;
+  export default {
+    components: { CouponItem, FjSticky, ListContainer, NavBar, FjSticky, NavBar },
+    props: {},
+    data() {
+      return {
+        title: '领券中心',
+        tabs: [
+          {
+            name: '全部',
+          },
+          {
+            name: '满减券',
+          },
+          {
+            name: '无门槛优惠券',
+          },
+        ],
+        tab: 0,
+        showLoading: true,
+        finished: false,
+        list: [],
+        page: 1,
+      };
     },
-  },
-};
+    mounted() {
+      setTimeout(() => {
+        this.list = couponList;
+      }, 1500);
+    },
+    onReachBottom() {
+      if (this.page >= 2) {
+        this.finished = true;
+        return;
+      }
+      this.finished = false;
+
+      setTimeout(() => {
+        this.list = this.list.concat(this.list);
+        this.page++;
+      }, 1500);
+    },
+    methods: {
+      onChangeTab(val) {
+        this.tab = val.index;
+        this.list = couponList;
+        this.page = 1;
+      },
+    },
+  };
 </script>
 
 <style scoped lang="less">
-.ml-29 {
-  margin-left: 58rpx;
-}
-.page {
-  background-color: #ffffff;
-  width: 100vw;
-  height: 100vh;
-}
-.main {
-  padding: 42rpx 0;
-}
-.banner-image-wrapper {
-  margin: 0 32rpx;
-  overflow: hidden;
-  border-radius: 16rpx;
-  height: 232rpx;
-}
-.banner-image {
-  width: 100%;
-  height: 100%;
-}
-.list-item {
-  margin: 0 32rpx;
-  margin-top: 20rpx;
-}
-.list-item:first-child {
-  margin-top: 0;
-}
-.scan-code {
-  padding: 26rpx 0;
-  background-color: #030305;
-  border-radius: 50%;
-  width: 108rpx;
-  position: fixed;
-  right: 32rpx;
-  bottom: 150rpx;
-}
-.ai-outflow {
-  width: 56rpx;
-  height: 56rpx;
-}
-
-.line-tabs {
-  /deep/ .u-tabs {
-    padding: 0 14rpx;
+  .ml-29 {
+    margin-left: 58rpx;
+  }
+  .page {
     background-color: #ffffff;
+    width: 100vw;
+    height: 100vh;
+  }
+  .main {
+    padding: 42rpx 0;
+  }
+  .banner-image-wrapper {
+    margin: 0 32rpx;
+    overflow: hidden;
+    border-radius: 16rpx;
+    height: 232rpx;
+  }
+  .banner-image {
+    width: 100%;
+    height: 100%;
+  }
+  .list-item {
+    margin: 0 32rpx;
+    margin-top: 20rpx;
+  }
+  .list-item:first-child {
+    margin-top: 0;
+  }
+  .scan-code {
+    padding: 26rpx 0;
+    background-color: #030305;
+    border-radius: 50%;
+    width: 108rpx;
+    position: fixed;
+    right: 32rpx;
+    bottom: 150rpx;
+  }
+  .ai-outflow {
+    width: 56rpx;
+    height: 56rpx;
   }
 
-  /deep/ .u-tabs__wrapper__nav__item {
-    height: 36px;
-  }
+  .line-tabs {
+    /deep/ .u-tabs {
+      padding: 0 14rpx;
+      background-color: #ffffff;
+    }
 
-  /deep/ .u-tabs__wrapper__nav__line {
-    background: linear-gradient(
-      90deg,
-      #e9b81b 0%,
-      #e9b81b00 97.2%
-    ) !important;
-    opacity: 0.7 !important;
-    border-radius: 10rpx !important;
-    bottom: 12rpx !important;
+    /deep/ .u-tabs__wrapper__nav__item {
+      height: 36px;
+    }
+
+    /deep/ .u-tabs__wrapper__nav__line {
+      background: linear-gradient(90deg, #e9b81b 0%, #e9b81b00 97.2%) !important;
+      opacity: 0.7 !important;
+      border-radius: 10rpx !important;
+      bottom: 12rpx !important;
+    }
   }
-}
 </style>
