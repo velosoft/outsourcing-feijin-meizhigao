f9312985ca4156414d8ed2bdd343773aea205416
garyxdh <garyxdh@gmail.com>
2023-12-25 13:44:16 +0800

    bugfix: 修复活动页中的样式bug，multitabs 支持横向滚动，list item 价格对齐

--- pages/activity/activity.vue
+++ pages/activity/activity.vue
@@ -10,21 +10,10 @@
           :tabs="multTabs"
           @change="onMultiCardTabs"
         ></multi-card-tabs>
-        <activity-status-waiting
-          v-if="activityStatus === 0"
-          class="self-center"
-        />
+        <activity-status-waiting v-if="activityStatus === 0" class="self-center" />
         <activity-status-running v-if="activityStatus === 1" />
-        <activity-status-expired
-          v-if="activityStatus === 2"
-          class="self-center"
-        />
-        <card-tabs
-          class="tabs-wraper"
-          :value="value"
-          :tabs="tabs"
-          @change="onCardTabs"
-        ></card-tabs>
+        <activity-status-expired v-if="activityStatus === 2" class="self-center" />
+        <card-tabs class="tabs-wraper" :value="value" :tabs="tabs" @change="onCardTabs"></card-tabs>
         <view class="flex-col">
           <list-container
             class="list-container-bg"
@@ -51,194 +40,215 @@
 </template>
 
 <script>
-import NavBar from "@/components/NavBar/NavBar.vue";
-import ListContainer from "@/components/ListContainer/ListContainer.vue";
-import MultiCardTabs from "./components/MultiCardTabs.vue";
-import CardTabs from "./components/CardTabs.vue";
-import ActivityShopItem from "./components/ActivityShopItem.vue";
-import ActivityStatusWaiting from "./components/ActivityStatusWaiting.vue";
-import ActivityStatusRunning from "./components/ActivityStatusRunning.vue";
-import ActivityStatusExpired from "./components/ActivityStatusExpired.vue";
-import { productList, serviceList, courseList } from "@/mock/shopList";
+  import NavBar from '@/components/NavBar/NavBar.vue';
+  import ListContainer from '@/components/ListContainer/ListContainer.vue';
+  import MultiCardTabs from './components/MultiCardTabs.vue';
+  import CardTabs from './components/CardTabs.vue';
+  import ActivityShopItem from './components/ActivityShopItem.vue';
+  import ActivityStatusWaiting from './components/ActivityStatusWaiting.vue';
+  import ActivityStatusRunning from './components/ActivityStatusRunning.vue';
+  import ActivityStatusExpired from './components/ActivityStatusExpired.vue';
+  import { productList, serviceList, courseList } from '@/mock/shopList';
 
-export default {
-  components: {
-    NavBar,
-    CardTabs,
-    MultiCardTabs,
-    ListContainer,
-    ActivityShopItem,
-    ActivityStatusWaiting,
-    ActivityStatusRunning,
-    ActivityStatusExpired,
-  },
-  props: {},
-  data() {
-    return {
-      multTabs: [
-        {
-          status: 2,
-          time: "10:00",
-          label: "已结束",
-        },
-        {
-          status: 1,
-          time: "11:00",
-          label: "抢购中",
-        },
-        {
-          status: 0,
-          time: "12:00",
-          label: "即将开始",
-        },
-        {
-          status: 0,
-          time: "13:00",
-          label: "即将开始",
-        },
-        {
-          status: 0,
-          time: "14:00",
-          label: "即将开始",
-        },
-      ],
-      multValue: 0,
-      activityStatus: 2,
-      title: "秒杀活动",
-      percentage: 40,
-      value: 0,
-      tabs: ["活动商品", "活动服务", "活动课程"],
-      page: 1,
-      showLoading: true,
-      finished: false,
-      list: productList,
-    };
-  },
-  onReachBottom() {
-    if (this.page >= 2) {
-      this.finished = true;
-      return;
-    }
-    this.finished = false;
-
-    setTimeout(() => {
-      this.list = this.list.concat(this.list);
-      this.page++;
-    }, 1500);
-  },
-  methods: {
-    onMultiCardTabs(val) {
-      // 事件处理方法
-      this.activityStatus = val.item.status;
+  export default {
+    components: {
+      NavBar,
+      CardTabs,
+      MultiCardTabs,
+      ListContainer,
+      ActivityShopItem,
+      ActivityStatusWaiting,
+      ActivityStatusRunning,
+      ActivityStatusExpired,
     },
-    onCardTabs(val) {
-      // 事件处理方法
-      this.value = val.index;
-      this.page = 1;
-
-      switch (this.value) {
-        case 0:
-          this.list = productList;
-          break;
-        case 1:
-          this.list = serviceList;
-          break;
-        case 2:
-          this.list = courseList;
-          break;
+    props: {},
+    data() {
+      return {
+        multTabs: [
+          {
+            status: 2,
+            time: '10:00',
+            label: '已结束',
+          },
+          {
+            status: 1,
+            time: '11:00',
+            label: '抢购中',
+          },
+          {
+            status: 0,
+            time: '12:00',
+            label: '即将开始',
+          },
+          {
+            status: 0,
+            time: '13:00',
+            label: '即将开始',
+          },
+          {
+            status: 0,
+            time: '14:00',
+            label: '即将开始',
+          },
+          {
+            status: 2,
+            time: '10:00',
+            label: '已结束',
+          },
+          {
+            status: 1,
+            time: '11:00',
+            label: '抢购中',
+          },
+          {
+            status: 0,
+            time: '12:00',
+            label: '即将开始',
+          },
+          {
+            status: 0,
+            time: '13:00',
+            label: '即将开始',
+          },
+          {
+            status: 0,
+            time: '14:00',
+            label: '即将开始',
+          },
+        ],
+        multValue: 0,
+        activityStatus: 2,
+        title: '秒杀活动',
+        percentage: 40,
+        value: 0,
+        tabs: ['活动商品', '活动服务', '活动课程'],
+        page: 1,
+        showLoading: true,
+        finished: false,
+        list: productList,
+      };
+    },
+    onReachBottom() {
+      if (this.page >= 2) {
+        this.finished = true;
+        return;
       }
+      this.finished = false;
+
+      setTimeout(() => {
+        this.list = this.list.concat(this.list);
+        this.page++;
+      }, 1500);
     },
-  },
-};
+    methods: {
+      onMultiCardTabs(val) {
+        // 事件处理方法
+        this.activityStatus = val.item.status;
+      },
+      onCardTabs(val) {
+        // 事件处理方法
+        this.value = val.index;
+        this.page = 1;
+
+        switch (this.value) {
+          case 0:
+            this.list = productList;
+            break;
+          case 1:
+            this.list = serviceList;
+            break;
+          case 2:
+            this.list = courseList;
+            break;
+        }
+      },
+    },
+  };
 </script>
 
 <style scoped lang="less">
-.page {
-  background-color: #f71f20;
-  width: 100%;
-  overflow-y: auto;
-  overflow-x: hidden;
-  height: 100%;
-  .container {
+  .page {
+    background-color: #f71f20;
+    width: 100%;
     overflow-y: auto;
-    .banner {
-      width: 100vw;
-      height: 73.6vw;
-    }
-    .line-tabs {
-      padding: 0 20rpx 56rpx;
-      .card-tabs-wrap {
-        position: relative;
+    overflow-x: hidden;
+    height: 100%;
+    .container {
+      overflow-y: auto;
+      .banner {
+        width: 100vw;
+        height: 73.6vw;
       }
-      .btn-tips {
-        margin-top: 24rpx;
-        padding: 16rpx 24rpx;
-        background-image: linear-gradient(180deg, #fe6733 0%, #f72222 100%);
-        border-radius: 100rpx;
-        .tips-text {
-          width: 280.58rpx;
-          height: 24.04rpx;
+      .line-tabs {
+        padding: 0 20rpx 56rpx;
+        .card-tabs-wrap {
+          position: relative;
         }
-        .count-down-wrap {
-          .count-down-label {
-            font-size: 24rpx;
-            font-family: 苹方;
-            line-height: 24rpx;
-            color: #ffffff;
+        .btn-tips {
+          margin-top: 24rpx;
+          padding: 16rpx 24rpx;
+          background-image: linear-gradient(180deg, #fe6733 0%, #f72222 100%);
+          border-radius: 100rpx;
+          .tips-text {
+            width: 280.58rpx;
+            height: 24.04rpx;
           }
-          .count-down-con {
-            flex-shrink: 0;
-            .number-box {
-              padding: 8rpx 0;
-              background-image: linear-gradient(
-                180deg,
-                #b4352e 0%,
-                #ce3933 100%
-              );
-              border-radius: 4rpx;
-              width: 34rpx;
-              height: 34rpx;
-              .time-font {
-                font-size: 20rpx;
-                font-family: PingFang SC Bold;
-                line-height: 20rpx;
-                font-weight: 700;
-                color: #ffffff;
-              }
-              .time-text {
-                font-size: 22rpx;
-                line-height: 22rpx;
-              }
-            }
-            .colon {
+          .count-down-wrap {
+            .count-down-label {
               font-size: 24rpx;
               font-family: 苹方;
               line-height: 24rpx;
-              color: #ffc8a7;
+              color: #ffffff;
+            }
+            .count-down-con {
+              flex-shrink: 0;
+              .number-box {
+                padding: 8rpx 0;
+                background-image: linear-gradient(180deg, #b4352e 0%, #ce3933 100%);
+                border-radius: 4rpx;
+                width: 34rpx;
+                height: 34rpx;
+                .time-font {
+                  font-size: 20rpx;
+                  font-family: PingFang SC Bold;
+                  line-height: 20rpx;
+                  font-weight: 700;
+                  color: #ffffff;
+                }
+                .time-text {
+                  font-size: 22rpx;
+                  line-height: 22rpx;
+                }
+              }
+              .colon {
+                font-size: 24rpx;
+                font-family: 苹方;
+                line-height: 24rpx;
+                color: #ffc8a7;
+              }
             }
           }
         }
-      }
-      .tabs-wraper {
-        position: relative;
-        margin-top: 48rpx;
-      }
-      .list {
-        padding: 20rpx 20rpx 0;
-        border-radius: 0rpx 0rpx 16rpx 16rpx;
-
-        .list-item {
-          margin-top: 16rpx;
+        .tabs-wraper {
+          position: relative;
+          margin-top: 48rpx;
         }
+        .list {
+          padding: 20rpx 20rpx 0;
+          border-radius: 0rpx 0rpx 16rpx 16rpx;
 
-        .list-item:first-child {
-          margin-top: 0;
+          .list-item {
+            margin-top: 16rpx;
+          }
+
+          .list-item:first-child {
+            margin-top: 0;
+          }
         }
-      }
-      .list-container-bg {
-        background-image: linear-gradient(86.8deg, #fbebbc 4.9%, #fefde2 95.3%);
+        .list-container-bg {
+          background-image: linear-gradient(86.8deg, #fbebbc 4.9%, #fefde2 95.3%);
+        }
       }
     }
   }
-}
 </style>
--- pages/activity/components/MultiCardTabs.vue
+++ pages/activity/components/MultiCardTabs.vue
@@ -1,195 +1,217 @@
 <template>
   <view class="flex-col justify-start relative">
-    <view class="flex-row justify-between items-center card-tabs">
-      <view
-        class="flex-col items-center justify-center tabs-item"
-        v-for="(v, i) in tabs"
-        :key="i"
-        :style="{
-          flex: 1,
-        }"
-        @click="change(i, v)"
-      >
-        <text class="item-font item-time">{{ v.time }}</text>
-        <text class="item-font">{{ v.label }}</text>
+    <view class="bg-abs"></view>
+    <scroll-view class="card-tabs" :scroll-x="true">
+      <view class="flex-row justify-start items-center tabs-wrapper">
+        <view
+          class="flex-col items-center justify-center tabs-item shrink-0 tab-item"
+          v-for="(v, i) in tabs"
+          :key="i"
+          :style="{
+            flex: 1,
+          }"
+          @click="change(i, v)"
+        >
+          <text class="item-font item-time">{{ v.time }}</text>
+          <text class="item-font">{{ v.label }}</text>
+        </view>
       </view>
-    </view>
-    <view
-      class="flex-col items-center tabs-item-active pos"
-      :style="{
-        width: currentWidth + 'px',
-        left: lineLeft + 'px',
-      }"
-    >
-      <text class="item-time-active">{{ tabs[current].time }}</text>
-      <text class="item-label-active mt-4">{{ tabs[current].label }}</text>
-    </view>
+      <view
+        class="flex-col items-center tabs-item-active pos"
+        :style="{
+          width: currentWidth + 'px',
+          left: lineLeft + 'px',
+        }"
+      >
+        <text class="item-time-active">{{ tabs[current].time }}</text>
+        <text class="item-label-active mt-4">{{ tabs[current].label }}</text>
+      </view>
+    </scroll-view>
   </view>
 </template>
 
 <script>
-export default {
-  props: {
-    value: {
-      type: [Number, String],
-      default: 0,
-    },
-    tabs: {
-      type: Array,
-      default() {
-        return [
-          {
-            time: "10:00",
-            label: "已结束",
-          },
-          {
-            time: "11:00",
-            label: "抢购中",
-          },
-          {
-            time: "12:00",
-            label: "即将开始",
-          },
-          {
-            time: "13:00",
-            label: "即将开始",
-          },
-          {
-            time: "14:00",
-            label: "即将开始",
-          },
-        ];
+  export default {
+    props: {
+      value: {
+        type: [Number, String],
+        default: 0,
+      },
+      tabs: {
+        type: Array,
+        default() {
+          return [
+            {
+              time: '10:00',
+              label: '已结束',
+            },
+            {
+              time: '11:00',
+              label: '抢购中',
+            },
+            {
+              time: '12:00',
+              label: '即将开始',
+            },
+            {
+              time: '13:00',
+              label: '即将开始',
+            },
+            {
+              time: '14:00',
+              label: '即将开始',
+            },
+          ];
+        },
+      },
+      field: {
+        type: String,
+        default: '',
       },
     },
-    field: {
-      type: String,
-      default: "",
+    data() {
+      return {
+        currentWidth: 0, // 当前选项的宽度
+        lineLeft: 0, // 滑块距离左侧的位置
+        active: 0,
+      };
     },
-  },
-  data() {
-    return {
-      currentWidth: 0, // 当前选项的宽度
-      lineLeft: 0, // 滑块距离左侧的位置
-      active: 0,
-    };
-  },
-  computed: {
-    current() {
-      const { active } = this;
-      let index = active || 0;
-      return index;
+    computed: {
+      current() {
+        const { active } = this;
+        let index = active || 0;
+        return index;
+      },
     },
-  },
-  watch: {
-    value: {
-      handler(newVal) {
-        if (this.active != newVal) {
-          this.active = newVal;
+    watch: {
+      value: {
+        handler(newVal) {
+          if (this.active != newVal) {
+            this.active = newVal;
+          }
+        },
+        immediate: true,
+      },
+      current(newVal) {
+        this.$nextTick(() => {
+          this.getTabItemWidth();
+        });
+      },
+      tabs(newVal) {
+        this.$nextTick(() => {
+          this.getTabItemWidth();
+        });
+      },
+    },
+
+    mounted() {
+      this.$nextTick(() => {
+        this.getTabItemWidth();
+      });
+    },
+
+    methods: {
+      // 切换事件
+      change(index, item) {
+        if (this.active !== index) {
+          this.active = index;
+          this.$emit('change', { index, item });
         }
       },
-      immediate: true,
-    },
-    current(newVal) {
-      this.$nextTick(() => {
-        this.getTabItemWidth();
-      });
-    },
-    tabs(newVal) {
-      this.$nextTick(() => {
-        this.getTabItemWidth();
-      });
-    },
-  },
-
-  mounted() {
-    this.$nextTick(() => {
-      this.getTabItemWidth();
-    });
-  },
-
-  methods: {
-    // 切换事件
-    change(index, item) {
-      if (this.active !== index) {
-        this.active = index;
-        this.$emit("change", {index, item});
-      }
-    },
-    // 获取左移位置
-    getTabItemWidth() {
-      let query = uni.createSelectorQuery().in(this);
-      query
-        .selectAll(".tabs-item")
-        .boundingClientRect((data) => {
-          if (!data) {
-            return;
-          }
-          let lineLeft = 0;
-          let currentWidth = 0;
-
-          if (data) {
-            for (let i = 0; i < data.length; i++) {
-              if (i < this.current) {
-                lineLeft += data[i].width;
-              } else if (i == this.current) {
-                currentWidth = data[i].width;
-              } else {
-                break;
-              }
+      // 获取左移位置
+      getTabItemWidth() {
+        let query = uni.createSelectorQuery().in(this);
+        query
+          .selectAll('.tabs-item')
+          .boundingClientRect((data) => {
+            if (!data) {
+              return;
             }
-            this.currentWidth = currentWidth;
-            this.lineLeft = lineLeft;
-          }
-        })
-        .exec();
+            let lineLeft = 0;
+            let currentWidth = 0;
+
+            if (data) {
+              for (let i = 0; i < data.length; i++) {
+                if (i < this.current) {
+                  lineLeft += data[i].width;
+                } else if (i == this.current) {
+                  currentWidth = data[i].width;
+                } else {
+                  break;
+                }
+              }
+              this.currentWidth = currentWidth;
+              this.lineLeft = lineLeft;
+            }
+          })
+          .exec();
+      },
     },
-  },
-};
+  };
 </script>
 
 <style scoped>
-.group_2 {
-  padding-top: 8rpx;
-}
-.card-tabs {
-  padding: 16rpx 0;
-  background-color: #eb615b;
-  border-radius: 16rpx 16rpx 0rpx 0rpx;
-}
-.item-font {
-  font-size: 20rpx;
-  font-family: 苹方;
-  line-height: 20rpx;
-  color: #ffffff;
-}
-.item-time {
-  font-size: 22rpx;
-  line-height: 22rpx;
-}
-.tabs-item-active {
-  padding: 16rpx 0;
-  background-image: linear-gradient(180deg, #fcf1ce 0%, #faebae 100%);
-  border-radius: 16rpx 16rpx 0rpx 0rpx;
-  width: 142rpx;
-  border: solid 1rpx #ffffff;
-}
-.pos {
-  position: absolute;
-  left: 0;
-  bottom: 0;
-}
-.item-time-active {
-  font-family: PingFang SC Bold;
-  font-weight: 700;
-  color: #030305;
-  font-size: 26rpx;
-  line-height: 26rpx;
-}
-.item-label-active {
-  font-size: 20rpx;
-  font-family: PingFang SC Bold;
-  line-height: 20rpx;
-  font-weight: 700;
-  color: #030305;
-}
+  .group_2 {
+    padding-top: 8rpx;
+  }
+
+  .item-font {
+    font-size: 20rpx;
+    font-family: 苹方;
+    line-height: 20rpx;
+    color: #ffffff;
+  }
+  .item-time {
+    font-size: 22rpx;
+    line-height: 22rpx;
+  }
+
+  .pos {
+    position: absolute;
+    left: 0;
+    top: 0;
+  }
+  .item-time-active {
+    font-family: PingFang SC Bold;
+    font-weight: 700;
+    color: #030305;
+    font-size: 26rpx;
+    line-height: 26rpx;
+  }
+  .item-label-active {
+    font-size: 20rpx;
+    font-family: PingFang SC Bold;
+    line-height: 20rpx;
+    font-weight: 700;
+    color: #030305;
+  }
+  .card-tabs {
+    height: 80rpx;
+  }
+  .tabs-wrapper {
+    height: 70rpx;
+    margin-top: 10rpx;
+  }
+  .tab-item {
+    min-width: 142rpx;
+  }
+  .bg-abs {
+    position: absolute;
+    height: 70rpx;
+    width: 100%;
+    top: 10rpx;
+    left: 0rpx;
+    background-color: #eb615b;
+    border-radius: 16rpx 16rpx 0rpx 0rpx;
+  }
+  .tabs-item-active {
+    padding: 10rpx 8rpx;
+    background-image: linear-gradient(180deg, #fcf1ce 0%, #faebae 100%);
+    border-radius: 16rpx 16rpx 0rpx 0rpx;
+    width: 142rpx;
+    border: solid 1rpx #ffffff;
+    min-width: 142rpx;
+    height: 80rpx;
+    border-bottom: unset;
+  }
 </style>
