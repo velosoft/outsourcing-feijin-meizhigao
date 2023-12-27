8eb0210d7f38c1cc07ce846216d5fb9601f7c626
garyxdh <garyxdh@gmail.com>
2023-12-27 22:56:34 +0800

    feat: 优化【购物车】页的推荐区域

--- pages/cart/components/Recommend/Recommend.vue
+++ pages/cart/components/Recommend/Recommend.vue
@@ -1,11 +1,11 @@
 <template>
   <view class="flex-col wrap">
-    <view class="flex-row justify-center recommend-wrap">
+    <view class="flex-row justify-center items-center recommend-wrap">
       <image class="icon-recommend" src="/static/images/icon_cart_recommend_left.png" />
       <text class="recommend-title">你可能还喜欢</text>
       <image class="icon-recommend" src="/static/images/icon_cart_recommend_right.png" />
     </view>
-    <DoubleList class="mt-10" :items="list"></DoubleList>
+    <double-list class="mt-10" :items="list"></double-list>
   </view>
 </template>
 
@@ -17,7 +17,6 @@
     components: { DoubleList },
     data() {
       return {
-        items: '',
         list: productList,
       };
     },
@@ -37,7 +36,7 @@
       }
       .recommend-title {
         margin: 0 12rpx;
-        color: #030305;
+        color: #111111;
         font-size: 32rpx;
         font-family: PingFangSC;
         line-height: 32rpx;
