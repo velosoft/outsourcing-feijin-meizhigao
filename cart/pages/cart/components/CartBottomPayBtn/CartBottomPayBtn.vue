524e01134b4f136bfb7af6ba5ef2e35f5fcc5c30
Ronin <42492283+joshuabsyxzry@users.noreply.github.com>
2023-12-28 11:44:05 +0800

    feat:优化【提交订单页】底部合计字体族

--- cart/pages/cart/components/CartBottomPayBtn/CartBottomPayBtn.vue
+++ cart/pages/cart/components/CartBottomPayBtn/CartBottomPayBtn.vue
@@ -3,7 +3,7 @@
     <view class="fixed-bottom-safe flex-row justify-between items-center wrap">
       <view class="flex-col">
         <view class="flex-row items-end self-stretch">
-          <text class="tatal">合计</text>
+          <text class="total">合计</text>
           <text class="price">￥599.00</text>
         </view>
         <text class="self-start coupon-num mt-8">已优惠￥85.00</text>
@@ -40,10 +40,10 @@
       border-radius: 16rpx 16rpx 0rpx 0rpx;
       box-shadow: 0rpx -6rpx 10rpx #0000000a;
       z-index: 1000;
-      .tatal {
+      .total {
         color: #111111;
         font-size: 24rpx;
-        font-family: 苹方;
+        font-family: PingFang SC;
         line-height: 24rpx;
       }
       .price {
