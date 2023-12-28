57b04586b4c76c209d31078294ff10a1ae9ef2ad
Ronin <42492283+joshuabsyxzry@users.noreply.github.com>
2023-12-28 11:38:44 +0800

    feat:优化提交订单页底部按钮和文本样式

--- cart/pages/cart/components/CartBottomPayBtn/CartBottomPayBtn.vue
+++ cart/pages/cart/components/CartBottomPayBtn/CartBottomPayBtn.vue
@@ -2,13 +2,15 @@
   <view class="flex-col footer-wrap">
     <view class="fixed-bottom-safe flex-row justify-between items-center wrap">
       <view class="flex-col">
-        <view class="flex-row items-center self-stretch">
+        <view class="flex-row items-end self-stretch">
           <text class="tatal">合计</text>
           <text class="price">￥599.00</text>
         </view>
         <text class="self-start coupon-num mt-8">已优惠￥85.00</text>
       </view>
-      <u-button text="确认提交" type="primary" shape="circle" @click="onClick"></u-button>
+      <view class="flex-col items-center justify-center button" @click="onClick">
+        <text class="btn-text">确认提交</text>
+      </view>
     </view>
   </view>
 </template>
@@ -39,23 +41,35 @@
       box-shadow: 0rpx -6rpx 10rpx #0000000a;
       z-index: 1000;
       .tatal {
-        color: #030305;
+        color: #111111;
         font-size: 24rpx;
         font-family: 苹方;
         line-height: 24rpx;
       }
       .price {
-        color: #030305;
+        color: #111111;
         font-size: 36rpx;
         font-family: HarmonyOS Sans SC;
         font-weight: 700;
         line-height: 36rpx;
       }
       .coupon-num {
-        color: #999999;
+        color: #6d6d6d;
         font-size: 22rpx;
         font-family: 苹方;
         line-height: 22rpx;
+      }
+    }
+    .button {
+      padding: 0 44rpx;
+      background-color: #b09053;
+      border-radius: 40rpx;
+      height: 80rpx;
+      .btn-text {
+        color: #ffffff;
+        font-size: 30rpx;
+        font-family: 苹方;
+        line-height: 80rpx;
       }
     }
   }
