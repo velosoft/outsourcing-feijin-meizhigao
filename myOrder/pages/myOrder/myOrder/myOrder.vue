d5837c8553049354ddd34303199e90e786ac4137
garyxdh <garyxdh@gmail.com>
2023-12-31 12:40:04 +0800

    feat: 优化【订单列表】页的 dom 写法

--- myOrder/pages/myOrder/myOrder/myOrder.vue
+++ myOrder/pages/myOrder/myOrder/myOrder.vue
@@ -1,15 +1,15 @@
 <template>
   <view class="flex-col page">
-    <NavBar :hasBack="true" :title="title" :fixed="true" :isShow="true" background="#ffffff"></NavBar>
+    <nav-bar :hasBack="true" :title="title" :fixed="true" :isShow="true" background="#ffffff"></nav-bar>
     <view class="flex-row justify-between items-center search-bar">
       <view class="flex-row items-center" @click="onClick">
         <text class="order-type-font order-type">{{ orderType }}</text>
         <image class="shrink-0 arrow-down ml-4" src="/myOrder/static/images/icon_down_arrow_01.png" />
       </view>
-      <SearchBar :value="value" placeholder="商品名称搜索" background="#f8f8f8"></SearchBar>
+      <search-bar :value="value" placeholder="商品名称搜索" background="#f8f8f8"></search-bar>
     </view>
     <view class="flex-col">
-      <FjSticky>
+      <fj-sticky>
         <u-tabs
           class="tabs-yellow-wide"
           :list="tabs"
@@ -26,7 +26,7 @@
           }"
           @change="onChangeStatus"
         ></u-tabs>
-      </FjSticky>
+      </fj-sticky>
       <list-container
         class="relative flex-auto order-container"
         :showEmpty="!orders.length"
@@ -38,28 +38,28 @@
       >
         <view class="flex-col orders">
           <view class="flex-col list-item mt-10" v-for="(item, index) in orders" :key="index">
-            <ProductOrderItem :order="item" v-if="orderType == '商品订单'"></ProductOrderItem>
-            <IntentionOrderItem :order="item" v-if="orderType == '意向订单'"></IntentionOrderItem>
-            <ServiceOrderItem :order="item" v-if="orderType == '服务订单'"></ServiceOrderItem>
+            <product-order-item :order="item" v-if="orderType == '商品订单'"></product-order-item>
+            <intention-order-item :order="item" v-if="orderType == '意向订单'"></intention-order-item>
+            <service-order-item :order="item" v-if="orderType == '服务订单'"></service-order-item>
           </view>
         </view>
       </list-container>
     </view>
     <u-popup :show="popupVisible" @close="onClose" mode="bottom" :round="12">
-      <PopOrderType @change="onChangeType"></PopOrderType>
+      <pop-order-type @change="onChangeType"></pop-order-type>
     </u-popup>
   </view>
 </template>
 
 <script>
   import FjSticky from '@/components/FjSticky.vue';
-  import IntentionOrderItem from '../../../pages/myOrder/components/IntentionOrderItem/IntentionOrderItem.vue';
+  import IntentionOrderItem from '@/myOrder/pages/myOrder/components/IntentionOrderItem/IntentionOrderItem.vue';
   import ListContainer from '@/components/ListContainer/ListContainer.vue';
   import NavBar from '@/components/NavBar/NavBar.vue';
-  import PopOrderType from '../../../pages/myOrder/components/PopOrderType/PopOrderType.vue';
-  import ProductOrderItem from '../../../pages/myOrder/components/ProductOrderItem/ProductOrderItem.vue';
+  import PopOrderType from '@/myOrder/pages/myOrder/components/PopOrderType/PopOrderType.vue';
+  import ProductOrderItem from '@/myOrder/pages/myOrder/components/ProductOrderItem/ProductOrderItem.vue';
   import SearchBar from '@/components/SearchBar';
-  import ServiceOrderItem from '../../../pages/myOrder/components/ServiceOrderItem/ServiceOrderItem.vue';
+  import ServiceOrderItem from '@/myOrder/pages/myOrder/components/ServiceOrderItem/ServiceOrderItem.vue';
   import { productOrders, productOrderStatus } from '@/mock/personal/orders';
   import { serviceOrderTabs, serviceOrders } from '@/mock/serviceOrder';
   import { intentionOrderTabs, intentionOrders } from '@/mock/intentionOrder';
@@ -78,9 +78,9 @@
     props: {},
     data() {
       return {
-        orderType: '商品订单',
+        orderType: '意向订单',
         orderStatus: '',
-        orders: productOrders,
+        orders: intentionOrders,
         title: '我的订单',
         popupVisible: false,
         value: '',
