16d00b29becf8da6bcf37879abbc54b44d8ce248
garyxdh <garyxdh@gmail.com>
2023-12-31 12:38:30 +0800

    feat: 优化【意向订单】的【确认报价】页样式

--- myOrder/pages/myOrder/intentionConfirmQuote/intentionConfirmQuote.vue
+++ myOrder/pages/myOrder/intentionConfirmQuote/intentionConfirmQuote.vue
@@ -1,6 +1,6 @@
 <template>
   <view class="flex-col page">
-    <NavBar :hasBack="true" :title="title" :fixed="true" :isShow="true" background="#f8f8f8"></NavBar>
+    <nav-bar :hasBack="true" :title="title" :fixed="true" :isShow="true" background="#f8f8f8"></nav-bar>
     <view class="flex-col flex-1 body">
       <view class="flex-col card">
         <u-cell class="cell-in-gray-card-14" title="查看方案" value="查看方案" :border="false">
@@ -15,7 +15,7 @@
             <image class="icon-arrow ml-4" src="/myOrder/static/images/icon_golden_right_arrow.png" />
           </view>
         </u-cell>
-        <view class="flex-row justify-between images mt-18">
+        <view class="flex-row justify-start gap-8 hidden-x mt-18">
           <image
             class="image"
             src="/myOrder/static/images/mock_1d10cf01926c2c71.png"
@@ -25,9 +25,9 @@
         </view>
         <u-cell class="cell-in-gray-card-14 mt-18" title="总金额（含运费）" value="￥2000" :border="false"></u-cell>
         <u-cell class="cell-in-gray-card-14 mt-18" title="人工优惠减免" value="￥200" :border="false"></u-cell>
-        <view class="divider mt-18"></view>
+        <view class="divider mt-28"></view>
         <u-cell
-          class="cell-in-gray-card-14 cell-value-sum mt-18"
+          class="cell-in-gray-card-14 cell-value-sum mt-24"
           title="最终报价金额"
           value="￥6800"
           :border="false"
@@ -39,13 +39,13 @@
         <text class="tip-text">2、请确认报价金额和领队口头报价是否一致</text>
         <text class="tip-text">3、确认报价并支付后，您可在意向订单中继续了解订单进度</text>
       </view>
-      <text class="self-center requotation" @click="gotoRequotation">当前报价有异，我申请重新报价</text>
+      <text class="self-center mt-130 requotation" @click="gotoRequotation">当前报价有异，我申请重新报价</text>
       <view class="flex-col items-center btn text-wrapper" @click="gotoSubmitPayment">
         <text class="btn-text btn-font">确认并前往支付</text>
       </view>
     </view>
     <u-popup :show="showProducts" @close="onCloseProducts" mode="bottom" :round="12" :closeable="true">
-      <PopProductInfo></PopProductInfo>
+      <pop-product-info></pop-product-info>
     </u-popup>
   </view>
 </template>
@@ -55,12 +55,15 @@
   import PopProductInfo from '@/myOrder/pages/myOrder/components/PopProductInfo/PopProductInfo.vue';
 
   export default {
+    options: {
+      styleIsolation: 'shared',
+    },
     components: { NavBar, PopProductInfo },
     props: {},
     data() {
       return {
         title: '确认报价',
-        items: [null, null, null, null, null],
+        items: [null, null, null, null, null, null, null],
         showProducts: false,
       };
     },
@@ -84,7 +87,7 @@
   };
 </script>
 
-<style scoped lang="css">
+<style scoped lang="less">
   .page {
     background-color: #f8f8f8;
     width: 100%;
@@ -97,7 +100,7 @@
     overflow-y: auto;
   }
   .card {
-    padding: 24rpx 24rpx 32rpx;
+    padding: 24rpx 24rpx 40rpx;
     background-color: #f0f0f0;
     border-radius: 8rpx;
   }
@@ -110,13 +113,11 @@
     font-size: 28rpx;
     line-height: 40rpx;
     color: #b09053;
+    font-weight: 400;
   }
   .icon-arrow {
     width: 8rpx;
     height: 14rpx;
-  }
-  .images {
-    margin-right: 4rpx;
   }
   .image {
     border-radius: 16rpx;
@@ -129,20 +130,22 @@
     color: #6d6d6d;
   }
   .divider {
-    border: 2rpx solid #111111;
+    border-top: 2rpx solid #111111;
   }
   .tip-title {
-    color: #2d2e32;
+    color: #111111;
+  }
+  .mt-130 {
+    margin-top: 260rpx;
   }
   .requotation {
-    margin-top: 76rpx;
     color: #b09053;
     font-size: 24rpx;
     line-height: 34rpx;
   }
   .btn {
     align-self: stretch;
-    margin: 32rpx 16rpx 0;
+    margin: 40rpx 16rpx 0;
   }
   .text-wrapper {
     padding: 28rpx 0;
@@ -152,5 +155,15 @@
   .btn-font {
     color: #ffffff;
     font-weight: 500;
+  }
+
+  .cell-in-gray-card-14 {
+    /deep/ .u-cell__title-text {
+      font-weight: 400;
+    }
+
+    /deep/ .u-cell__value {
+      font-family: HarmonyOS_Sans_Bold;
+    }
   }
 </style>
