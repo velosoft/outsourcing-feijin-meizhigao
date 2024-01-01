5a80e53b65f1311ec7fc117d0909da0e9814febb
dannnney <dannnney@gmail.com>
2024-01-01 18:55:22 +0800

    调整字重

--- myOrder/pages/myOrder/components/OrderInfoCard/OrderInfoCard.vue
+++ myOrder/pages/myOrder/components/OrderInfoCard/OrderInfoCard.vue
@@ -1,11 +1,11 @@
 <template>
   <collapse-title-card class="collapse-white-card" title="订单信息" :showCollapse="false">
     <view class="flex-col mt-16">
-      <u-cell class="cell-in-white-card" title="订单编号" :value="20220401786344763737" :border="false"></u-cell>
-      <u-cell class="cell-in-white-card mt-16" title="下单支付" value="￥10" :border="false"></u-cell>
-      <u-cell class="cell-in-white-card mt-16" title="支付方式" value="微信方式" :border="false"></u-cell>
-      <u-cell class="cell-in-white-card mt-16" title="提交时间" value="2023-09-2012:00:00" :border="false"></u-cell>
-      <u-cell class="cell-in-white-card mt-16" title="支付时间" value="2023-09-2012:00:00" :border="false"></u-cell>
+      <u-cell class="cell-in-white-card-weight" title="订单编号" :value="20220401786344763737" :border="false"></u-cell>
+      <u-cell class="cell-in-white-card-weight mt-16" title="下单支付" value="￥10" :border="false"></u-cell>
+      <u-cell class="cell-in-white-card-weight mt-16" title="支付方式" value="微信方式" :border="false"></u-cell>
+      <u-cell class="cell-in-white-card-weight mt-16" title="提交时间" value="2023-09-2012:00:00" :border="false"></u-cell>
+      <u-cell class="cell-in-white-card-weight mt-16" title="支付时间" value="2023-09-2012:00:00" :border="false"></u-cell>
     </view>
   </collapse-title-card>
 </template>
